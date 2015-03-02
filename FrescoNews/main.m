551aa8aa6c788cc199dd173d2f1f303d140c4233
Jason Gresh <jrgresh@gmail.com>
2015-03-02 11:52:26 -0500

    Initial Commit

---
+++ FrescoNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FrescoNews
+//
+//  Created by Jason Gresh on 3/2/15.
+//  Copyright (c) 2015 Fresco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
