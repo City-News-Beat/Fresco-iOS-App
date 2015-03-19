98cd4fa2a11645936151f2f2d34d7faa345a6b23
Jason Gresh <jrgresh@gmail.com>
2015-03-19 17:40:18 -0400

    Initial gallery work.

---
+++ FrescoNews/GalleryView.m
@@ -0,0 +1,15 @@
+//
+//  GalleryView.m
+//  FrescoNews
+//
+//  Created by Jason Gresh on 3/19/15.
+//  Copyright (c) 2015 Fresco. All rights reserved.
+//
+
+#import "GalleryView.h"
+
+@implementation GalleryView
+
+
+
+@end
