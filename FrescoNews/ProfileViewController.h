b2ac1aaffed29132ec527caf5b1365b92d387770
Zachary Mayberry <zachmayberry@gmail.com>
2015-04-01 14:20:37 -0500

    First

--- FrescoNews/ProfileViewController.h
+++ FrescoNews/ProfileViewController.h
@@ -1,13 +1,14 @@
 //
-//  ProfileViewController.h
+//  FullPageGalleryViewController.h
 //  FrescoNews
 //
-//  Created by Jason Gresh on 3/6/15.
+//  Created by Jason Gresh on 3/19/15.
 //  Copyright (c) 2015 Fresco. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-
+@class GalleryView, FRSStory;
 @interface ProfileViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet GalleryView *galleryView;
+@property (weak, nonatomic) FRSStory *story;
 @end
--- FrescoNews/ProfileViewController.m
+++ FrescoNews/ProfileViewController.m
@@ -1,20 +1,80 @@
 //
-//  ProfileViewController.m
+//  FullPageGalleryViewController.m
 //  FrescoNews
 //
-//  Created by Jason Gresh on 3/6/15.
+//  Created by Jason Gresh on 3/19/15.
 //  Copyright (c) 2015 Fresco. All rights reserved.
 //
 
 #import "ProfileViewController.h"
-#import "UIViewController+Additions.h"
+#import "StoryTableViewCell.h"
+#import "GalleryView.h"
+#import "FRSStory.h"
+#import "FRSGallery.h"
+#import "UIView+Additions.h"
+
+@interface ProfileViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@end
 
 @implementation ProfileViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        [self setup];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super initWithCoder:aDecoder]) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void)setup
+{
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self setFrescoImageHeader];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
 }
 
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return [self.story.galleries count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // since there is a section for every story
+    // and just one story per section
+    // the section will tell us the "row"
+    NSUInteger index = indexPath.section;
+    
+    FRSGallery *gallery = [self.story.galleries objectAtIndex:index];
+    
+    StoryTableViewCell *storyTableViewCell = [tableView dequeueReusableCellWithIdentifier:[StoryTableViewCell identifier] forIndexPath:indexPath];
+    
+    storyTableViewCell.gallery = gallery;
+    //[storyCell layoutIfNeeded];
+    
+    return storyTableViewCell;
+}
+
+
+#pragma mark - UITableViewDelegate
+//-(CGSize)tableView:(UITableView *)tableView s
 @end
