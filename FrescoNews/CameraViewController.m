2105ea82b51137df90557e5b02488d312c983aa7
Joshua C. Lerner <jlerner@gmail.com>
2015-04-01 18:41:14 -0400

    Automatically upload captured stills and video to CDN, currently S3

--- FrescoNews/CameraViewController.m
+++ FrescoNews/CameraViewController.m
@@ -246,8 +246,12 @@ static void * SessionRunningAndDeviceAuthorizedContext = &SessionRunningAndDevic
             // Turning OFF flash for video recording
             [CameraViewController setFlashMode:AVCaptureFlashModeOff forDevice:[[self videoDeviceInput] device]];
 
-            // Start recording to a temporary file.
+            NSError *writeError = nil;
             NSString *outputFilePath = [NSTemporaryDirectory() stringByAppendingPathComponent:[@"movie" stringByAppendingPathExtension:@"mov"]];
+            if ([[NSFileManager defaultManager] fileExistsAtPath:outputFilePath]) {
+                [[NSFileManager defaultManager] removeItemAtPath:outputFilePath error:&writeError];
+            }
+
             [[self movieFileOutput] startRecordingToOutputFileURL:[NSURL fileURLWithPath:outputFilePath] recordingDelegate:self];
         }
         else {
@@ -265,45 +269,52 @@ static void * SessionRunningAndDeviceAuthorizedContext = &SessionRunningAndDevic
         // Flash set to Auto for Still Capture
         [CameraViewController setFlashMode:AVCaptureFlashModeAuto forDevice:[[self videoDeviceInput] device]];
 
-        // Capture a still image.
         [[self stillImageOutput] captureStillImageAsynchronouslyFromConnection:[[self stillImageOutput] connectionWithMediaType:AVMediaTypeVideo] completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
-
             if (imageDataSampleBuffer) {
                 NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
                 UIImage *image = [[UIImage alloc] initWithData:imageData];
                 [[[ALAssetsLibrary alloc] init] writeImageToSavedPhotosAlbum:[image CGImage] orientation:(ALAssetOrientation)[image imageOrientation] completionBlock:^(NSURL *assetURL, NSError *error) {
                     NSData *jpegImageData = UIImageJPEGRepresentation(image, 1.0);
-                    NSString *myFilePath = [[NSBundle mainBundle] pathForResource:@"photo"
-                                                                           ofType:@"jpeg"];
-                    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
-                    NSString *directory = [NSString stringWithFormat:@"%@", [paths objectAtIndex:0]];
-                    NSString *photoPath = [directory stringByAppendingPathComponent:[NSString stringWithFormat:@"%@", [myFilePath lastPathComponent]]];
+                    NSString *photoPath = [NSTemporaryDirectory() stringByAppendingPathComponent:[@"photo" stringByAppendingPathExtension:@"jpeg"]];
 
                     NSError *writeError = nil;
                     if (![jpegImageData writeToFile:photoPath options:NSDataWritingFileProtectionNone error:&writeError]) {
                         [jpegImageData writeToFile:photoPath atomically:NO];
                     }
-
-                    AFAmazonS3Manager *s3Manager = [[AFAmazonS3Manager alloc] initWithAccessKeyID:@"AKIAJNXOI5QWV3MSTZVA"
-                                                                                           secret:@"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"];
-                    s3Manager.requestSerializer.region = AFAmazonS3USStandardRegion;
-                    s3Manager.requestSerializer.bucket = @"com.fresconews";
-                    [s3Manager putObjectWithFile:photoPath
-                                  destinationPath:[NSString stringWithFormat:@"/uploads/photo%@.jpeg", @((NSInteger)[[NSDate date] timeIntervalSince1970])]
-                                       parameters:nil
-                                         progress:^(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
-                                             NSLog(@"%f%% Uploaded", (totalBytesWritten / (totalBytesExpectedToWrite * 1.0f) * 100));
-                                         }
-                                          success:^(id responseObject) {
-                                              // NSLog(@"Upload Complete: %@", responseObject.URL);
-                                          }
-                                          failure:^(NSError *error) {
-                                              NSLog(@"Error: %@", error);
-                                          }];
+                    
+                    [self uploadToCDN:photoPath photo:YES];
                 }];
              }
         }];
     });
+}
+
+- (void)uploadToCDN:(NSString *)localPath photo:(BOOL)isPhoto
+{
+    NSString *destinationPath;
+    if (isPhoto) {
+        destinationPath = [NSString stringWithFormat:@"/uploads/photo%@.jpeg", @((NSInteger)[[NSDate date] timeIntervalSince1970])];
+    }
+    else {
+        destinationPath = [NSString stringWithFormat:@"/uploads/movie%@.mov", @((NSInteger)[[NSDate date] timeIntervalSince1970])];
+    }
+
+    AFAmazonS3Manager *s3Manager = [[AFAmazonS3Manager alloc] initWithAccessKeyID:@"AKIAJNXOI5QWV3MSTZVA"
+                                                                           secret:@"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"];
+    s3Manager.requestSerializer.region = AFAmazonS3USStandardRegion;
+    s3Manager.requestSerializer.bucket = @"com.fresconews";
+    [s3Manager putObjectWithFile:localPath
+                 destinationPath:destinationPath
+                      parameters:nil
+                        progress:^(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
+                            NSLog(@"%f%% Uploaded", (totalBytesWritten / (totalBytesExpectedToWrite * 1.0f) * 100));
+                        }
+                         success:^(id responseObject) {
+                             // NSLog(@"Upload Complete: %@", responseObject.URL);
+                         }
+                         failure:^(NSError *error) {
+                             NSLog(@"Error: %@", error);
+                         }];
 }
 
 - (IBAction)focusAndExposeTap:(UIGestureRecognizer *)gestureRecognizer
@@ -356,23 +367,28 @@ static void * SessionRunningAndDeviceAuthorizedContext = &SessionRunningAndDevic
 
 - (void)captureOutput:(AVCaptureFileOutput *)captureOutput didFinishRecordingToOutputFileAtURL:(NSURL *)outputFileURL fromConnections:(NSArray *)connections error:(NSError *)error
 {
-    if (error)
+    if (error) {
         NSLog(@"%@", error);
+    }
 
     [self setLockInterfaceRotation:NO];
+    
+    [self uploadToCDN:[NSTemporaryDirectory() stringByAppendingPathComponent:[@"movie" stringByAppendingPathExtension:@"mov"]] photo:NO];
 
     // Note the backgroundRecordingID for use in the ALAssetsLibrary completion handler to end the background task associated with this recording. This allows a new recording to be started, associated with a new UIBackgroundTaskIdentifier, once the movie file output's -isRecording is back to NO — which happens sometime after this method returns.
     UIBackgroundTaskIdentifier backgroundRecordingID = [self backgroundRecordingID];
     [self setBackgroundRecordingID:UIBackgroundTaskInvalid];
 
     [[[ALAssetsLibrary alloc] init] writeVideoAtPathToSavedPhotosAlbum:outputFileURL completionBlock:^(NSURL *assetURL, NSError *error) {
-        if (error)
+        if (error) {
             NSLog(@"%@", error);
+        }
 
         [[NSFileManager defaultManager] removeItemAtURL:outputFileURL error:nil];
 
-        if (backgroundRecordingID != UIBackgroundTaskInvalid)
+        if (backgroundRecordingID != UIBackgroundTaskInvalid) {
             [[UIApplication sharedApplication] endBackgroundTask:backgroundRecordingID];
+        }
     }];
 }
 
