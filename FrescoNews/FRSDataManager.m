b59c59425eba35c1d4e4f48aede05aca2d595945
Zachary Mayberry <zachmayberry@gmail.com>
2015-04-06 13:22:13 -0500

    Tweaks to caption

--- FrescoNews/FRSDataManager.m
+++ FrescoNews/FRSDataManager.m
@@ -273,7 +273,7 @@ static NSString * const kPersistedUserFilename = @"user.usr";
 #warning for video
 #pragma mark - For Video
 - (void)getHomeDataWithResponseBlock:(FRSAPIResponseBlock)responseBlock{
-    NSString *path = @"http://monorail.theburgg.com/fresco/home_data.json";
+    NSString *path = @"http://monorail.theburgg.com/fresco/home_data.json?type=stories";
     
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
     
