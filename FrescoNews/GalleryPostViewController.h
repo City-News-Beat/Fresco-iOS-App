179cc3e4fd5c0f9c94d0ad2a5ff97e2e580f95b4
Joshua C. Lerner <jlerner@gmail.com>
2015-04-19 15:41:18 -0400

    Hook-up Gallery Post view controller

---
+++ FrescoNews/GalleryPostViewController.h
@@ -0,0 +1,13 @@
+//
+//  GalleryPostViewController.h
+//  FrescoNews
+//
+//  Created by Joshua Lerner on 4/19/15.
+//  Copyright (c) 2015 Fresco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GalleryPostViewController : UIViewController
+
+@end
