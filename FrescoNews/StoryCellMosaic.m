5de5de7214d6c908d1f8d6b8646379c667fe37cc
Jason Gresh <jrgresh@gmail.com>
2015-04-20 18:37:53 -0400

    Story mosaic WIP

--- FrescoNews/StoryCellMosaic.m
+++ FrescoNews/StoryCellMosaic.m
@@ -97,6 +97,10 @@ static CGFloat const kInterImageGap = 1.0f;
         [thumbnailView setImageWithURL:[image cdnImageURL]];
         [self.contentView addSubview:thumbnailView];
         
+        /*
+        NSString *format = [NSString stringWithFormat:@"H:|-(%f)-view-(%f)-|", frame.origin.x, frame.origin.x + frame.size.width
+        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:format options:0 metrics:nil views:nil]]*/
+        
         thumbnailView.story_id = [self.story.storyID integerValue];
         thumbnailView.thumbSequence = i;
         [self setupTapHandlingForThumbnail:thumbnailView];
@@ -120,65 +124,6 @@ static CGFloat const kInterImageGap = 1.0f;
         }
         
         ++i;
-    }
-}
-
-- (void)layoutSubviewsOld
-{
-    for (UIView *view in self.contentView.subviews) {
-        if ([view isKindOfClass:[StoryThumbnailView class]]) {
-            [view removeFromSuperview];
-            //NSLog(@"Removing view -- we could optimize this to resize views");
-        }
-    }
-    
-    CGRect frame;
-    CGFloat x = 0.0f;
-    CGFloat y = 0.0f;
-    int rows = 1;
-    
-    self.constraintHeight.constant = kImageHeight;
-    
-    for (FRSGallery *gallery in self.story.galleries) {
-        for (int i = 0; i < [gallery.posts count]; ++i) {
-            // we don't want more than two rows of images
-            if (rows > 2)
-                break;
-            
-            FRSPost *post = [gallery.posts objectAtIndex:i];
-            CGFloat scale = kImageHeight / [post.largeImage.height floatValue];
-            CGFloat imageWidth = [post.largeImage.width floatValue] * scale;
-            
-            // make the image view
-            frame = CGRectMake(x, y, imageWidth, kImageHeight);
-            
-            // lay the view down
-            StoryThumbnailView *thumbnailView = [[StoryThumbnailView alloc] initWithFrame:frame];
-            [thumbnailView setImageWithURL:[post.largeImage cdnImageURL]];
-            [self.contentView addSubview:thumbnailView];
-            
-            thumbnailView.story_id = [self.story.storyID integerValue];
-            thumbnailView.thumbSequence = i;
-            [self setupTapHandlingForThumbnail:thumbnailView];
-            
-            // calculate offsets for the next iteration
-            x += imageWidth + kInterImageGap;
-            
-            // check for wrap
-            if (x > self.frame.size.width) {
-                ++rows;
-                y += kImageHeight + kInterImageGap;
-                self.constraintHeight.constant = kImageHeight * 2 + kInterImageGap;
-                [self updateConstraints];
-                x = 0.0f;
-                
-                // we almost always want to redo this image on the next row
-                // but check the edge case where the image is wider than the
-                // whole frame which would cause an endless loop
-                if (imageWidth + kInterImageGap < self.frame.size.width)
-                    --i;
-            }
-        }
     }
 }
 
