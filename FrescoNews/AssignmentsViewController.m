526f8cbe5a8390b9295c56d308d61c3af01dd14b
Zachary Mayberry <zachmayberry@gmail.com>
2015-04-21 16:24:12 -0500

    Closer to a perfect UI, but not quite yet

--- FrescoNews/AssignmentsViewController.m
+++ FrescoNews/AssignmentsViewController.m
@@ -13,6 +13,7 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *storyBreaksNotification;
 @property (weak, nonatomic) IBOutlet UIView *storyBreaksView;
+@property (weak, nonatomic) IBOutlet UIView *detailViewWrapper;
 
 @property (weak, nonatomic) IBOutlet UILabel *assignmentTitle;
 @property (weak, nonatomic) IBOutlet UILabel *assignmentTimeElapsed;
@@ -29,7 +30,18 @@
     
     [self setFrescoImageHeader];
     
+    // UI Values
     self.storyBreaksView.backgroundColor = [UIColor colorWithHex:[VariableStore sharedInstance].colorStoryBreaksBackground];
+    self.detailViewWrapper.layer.shadowColor = [[UIColor blackColor] CGColor];
+    self.detailViewWrapper.layer.shadowOpacity = 0.26;
+    self.detailViewWrapper.layer.shadowOffset = CGSizeMake(-1, 0);
+    
+    
+    // "Real Fake Data"
+    self.storyBreaksNotification.text = @"Click here to be notified when a story breaks in your area";
+    self.assignmentTitle.text= @"Pileup by Dame Tipping Primary School";
+    self.assignmentDescription.text = @"Six cars and one truck were involved in a major car accident on North Rd/B175. Eyewitness reports suggest at least three casualities; police and EMS have not confirmed.";
+    self.assignmentTimeElapsed.text = @"3m";
 }
 
 @end
