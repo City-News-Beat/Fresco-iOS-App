c44e8df56f544da4f164fb1d84b87f40f8b12fa1
Zachary Mayberry <zachmayberry@gmail.com>
2015-04-21 15:27:39 -0500

    Rename labels and removed duplicate outlets

--- FrescoNews/AssignmentsViewController.m
+++ FrescoNews/AssignmentsViewController.m
@@ -12,10 +12,11 @@
 @interface AssignmentsViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *storyBreaksNotification;
+
 @property (weak, nonatomic) IBOutlet UILabel *assignmentTitle;
 @property (weak, nonatomic) IBOutlet UILabel *assignmentTimeElapsed;
-@property (weak, nonatomic) IBOutlet MKMapView *assignmentMapView;
 @property (weak, nonatomic) IBOutlet UILabel *assignmentDescription;
+@property (weak, nonatomic) IBOutlet MKMapView *assignmentsMap;
 
 @end
 
