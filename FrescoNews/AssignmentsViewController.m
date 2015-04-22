f5c78d84081dadc546cc257e178be74cadad86a6
Zachary Mayberry <zachmayberry@gmail.com>
2015-04-22 11:43:07 -0500

    Improve VC code organization and adding additional effects to map for better demo

--- FrescoNews/AssignmentsViewController.m
+++ FrescoNews/AssignmentsViewController.m
@@ -30,13 +30,41 @@
     
     [self setFrescoImageHeader];
     
+    
+    [self tweakUI];
+    [self insertFakeData]; // Will likely delete
+    [self zoomDelayDemo]; // Will likely delete
+    
+    self.detailViewWrapper.
+    
+
+}
+
+- (void)tweakUI {
     // UI Values
     self.storyBreaksView.backgroundColor = [UIColor colorWithHex:[VariableStore sharedInstance].colorStoryBreaksBackground];
     self.detailViewWrapper.layer.shadowColor = [[UIColor blackColor] CGColor];
     self.detailViewWrapper.layer.shadowOpacity = 0.26;
     self.detailViewWrapper.layer.shadowOffset = CGSizeMake(-1, 0);
+}
+
+- (void)zoomDelayDemo {
+    // Zooming map after delay for effect
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.0001f, 0.0001f);
+    CLLocationCoordinate2D coordinate = {38.343128671, -90.980396261};
+    MKCoordinateRegion region = {coordinate, span};
     
+    MKCoordinateRegion regionThatFits = [self.assignmentsMap regionThatFits:region];
+    NSLog(@"Fit Region %f %f", regionThatFits.center.latitude, regionThatFits.center.longitude);
     
+    double delayInSeconds = 4.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self.assignmentsMap setRegion:regionThatFits animated:YES];
+    });
+}
+
+- (void)insertFakeData {
     // "Real Fake Data"
     self.storyBreaksNotification.text = @"Click here to be notified when a story breaks in your area";
     self.assignmentTitle.text= @"Pileup by Dame Tipping Primary School";
