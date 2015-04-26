23df2b0256d3b7935d747ca142592ff867c2703d
Joshua C. Lerner <jlerner@gmail.com>
2015-04-25 21:57:36 -0400

    Save location information for assets created in-app

---
+++ FrescoNews/NSDate+ISO.h
@@ -0,0 +1,16 @@
+//
+//  NSDate+ISO.h
+//  FrescoNews
+//
+//  Created by Joshua Lerner on 4/25/15.
+//  Copyright (c) 2015 Fresco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (ISO)
+
+- (NSString *)ISODate;
+- (NSString *)ISOTime;
+
+@end
