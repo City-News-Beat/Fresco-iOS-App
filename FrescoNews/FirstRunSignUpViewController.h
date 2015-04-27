d1c73d4ea95a905fe28b04540b1a063c3242095e
Zachary Mayberry <zachmayberry@gmail.com>
2015-04-27 11:57:49 -0500

    Remaining screens. Many tweaks left to make and constraints to double check.

---
+++ FrescoNews/FirstRunSignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstRunSignUpViewController.h
+//  FrescoNews
+//
+//  Created by Zachary Mayberry on 4/27/15.
+//  Copyright (c) 2015 Fresco. All rights reserved.
+//
+
+#import "FRSBaseViewController.h"
+
+@interface FirstRunSignUpViewController : FRSBaseViewController
+
+@end
