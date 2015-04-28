c5d52482c8015724703aa54a07ce433b16478528
Zachary Mayberry <zachmayberry@gmail.com>
2015-04-28 11:02:37 -0500

    New progress bars, top and bottom borders for field wrappers, taller main action buttons at bottom, and more overall clean up of layout and warnings/errors

--- FrescoNews/FirstRunAccountViewController.m
+++ FrescoNews/FirstRunAccountViewController.m
@@ -11,6 +11,7 @@
 @interface FirstRunAccountViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *facebookButton;
 @property (weak, nonatomic) IBOutlet UIButton *twitterButton;
+@property (weak, nonatomic) IBOutlet UIView *fieldsWrapper;
 
 @end
 
@@ -20,11 +21,28 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self styleButtons];
+    
+    [self addFieldsBorders];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)addFieldsBorders {
+    // Set thickness
+    NSInteger borderThickness = 1;
+    
+    UIView *topBorder = [UIView new];
+    topBorder.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.08];
+    topBorder.frame = CGRectMake(0, 0, self.fieldsWrapper.frame.size.width, borderThickness);
+    [self.fieldsWrapper addSubview:topBorder];
+    
+    UIView *bottomBorder = [UIView new];
+    bottomBorder.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.08];
+    bottomBorder.frame = CGRectMake(0, self.fieldsWrapper.frame.size.height - borderThickness, self.fieldsWrapper.frame.size.width, borderThickness);
+    [self.fieldsWrapper addSubview:bottomBorder];
 }
 
 - (void)styleButtons {
