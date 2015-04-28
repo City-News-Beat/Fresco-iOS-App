7c365affd35137955943e35cb6dd9fda06fa9c61
Zachary Mayberry <zachmayberry@gmail.com>
2015-04-28 15:24:59 -0500

    Fixing all fields border for all screens

--- FrescoNews/FirstRunSignUpViewController.m
+++ FrescoNews/FirstRunSignUpViewController.m
@@ -17,28 +17,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    [self addFieldsBorders];
+    // Do any additional setup after loading the view.];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)addFieldsBorders {
-    // Set thickness
-    NSInteger borderThickness = 1;
-    
-    UIView *topBorder = [UIView new];
-    topBorder.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.08];
-    topBorder.frame = CGRectMake(0, 0, self.fieldsWrapper.frame.size.width, borderThickness);
-    [self.fieldsWrapper addSubview:topBorder];
-    
-    UIView *bottomBorder = [UIView new];
-    bottomBorder.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.08];
-    bottomBorder.frame = CGRectMake(0, self.fieldsWrapper.frame.size.height - borderThickness, self.fieldsWrapper.frame.size.width, borderThickness);
-    [self.fieldsWrapper addSubview:bottomBorder];
 }
 
 /*
--- FrescoNews/FirstRunViewController.m
+++ FrescoNews/FirstRunViewController.m
@@ -24,28 +24,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self styleButtons];
-    
-    [self addFieldsBorders];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)addFieldsBorders {
-    // Set thickness
-    NSInteger borderThickness = 1;
-    
-    UIView *topBorder = [UIView new];
-    topBorder.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.08];
-    topBorder.frame = CGRectMake(0, 0, self.fieldsWrapper.frame.size.width, borderThickness);
-    [self.fieldsWrapper addSubview:topBorder];
-    
-    UIView *bottomBorder = [UIView new];
-    bottomBorder.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.08];
-    bottomBorder.frame = CGRectMake(0, self.fieldsWrapper.frame.size.height - borderThickness, self.fieldsWrapper.frame.size.width, borderThickness);
-    [self.fieldsWrapper addSubview:bottomBorder];
 }
 
 - (void)styleButtons {
