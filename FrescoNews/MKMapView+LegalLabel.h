8b3159251e579c9b77c5b1e5a9268884808cffca
Jason Gresh <jrgresh@gmail.com>
2015-04-29 15:48:34 -0400

    Better scroll overlay handling.

---
+++ FrescoNews/MKMapView+LegalLabel.h
@@ -0,0 +1,24 @@
+//
+//  MKMapView+LegalLabel.h
+//  FrescoNews
+//
+//  Created by Jason Gresh on 4/29/15.
+//  Copyright (c) 2015 Fresco. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface MKMapView (LegalLabel)
+
+typedef enum {
+    MKMapViewLegalLabelPositionBottomLeft = 0,
+    MKMapViewLegalLabelPositionBottomCenter = 1,
+    MKMapViewLegalLabelPositionBottomRight = 2,
+} MKMapViewLegalLabelPosition;
+
+@property (nonatomic, readonly) UILabel *legalLabel;
+
+- (void)offsetLegalLabel:(CGSize)distance;
+- (void)setLegalLabelCenter:(CGPoint)point;
+
+@end
