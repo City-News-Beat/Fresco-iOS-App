1bf299860594ae68a2598a58edd62dc8ac37e957
Zachary Mayberry <zachmayberry@gmail.com>
2015-04-30 09:57:10 -0500

    Transferring current state between machines

--- FrescoNews/FirstRunAccountViewController.m
+++ FrescoNews/FirstRunAccountViewController.m
@@ -12,6 +12,8 @@
 @property (weak, nonatomic) IBOutlet UIButton *facebookButton;
 @property (weak, nonatomic) IBOutlet UIButton *twitterButton;
 @property (weak, nonatomic) IBOutlet UIView *fieldsWrapper;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topVerticalSpaceConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomVerticalSpaceConstraint;
 
 @end
 
@@ -21,6 +23,27 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self styleButtons];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShowOrHide:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShowOrHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,6 +57,22 @@
     
     self.facebookButton.layer.cornerRadius = 8;
     self.facebookButton.clipsToBounds = YES;
+}
+
+- (void)keyboardWillShowOrHide:(NSNotification *)notification
+{
+    [UIView animateWithDuration:[notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue]
+                          delay:0
+                        options:[notification.userInfo[UIKeyboardAnimationCurveUserInfoKey] unsignedIntegerValue] animations:^{
+                            CGFloat height = 0;
+                            if ([notification.name isEqualToString:UIKeyboardWillShowNotification]) {
+                                height = -1 * [notification.userInfo[UIKeyboardFrameBeginUserInfoKey] CGRectValue].size.height;
+                            }
+                            
+                            self.topVerticalSpaceConstraint.constant = height;
+                            self.bottomVerticalSpaceConstraint.constant = -1 * height;
+                            [self.view layoutIfNeeded];
+                        } completion:nil];
 }
 
 /*
--- FrescoNews/FirstRunViewController.m
+++ FrescoNews/FirstRunViewController.m
@@ -15,6 +15,8 @@
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
 @property (weak, nonatomic) IBOutlet UIButton *signUpButton;
 @property (weak, nonatomic) IBOutlet UIView *fieldsWrapper;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topVerticalSpaceConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomVerticalSpaceConstraint;
 
 @end
 
@@ -24,6 +26,27 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self styleButtons];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShowOrHide:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShowOrHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -43,6 +66,22 @@
     
     self.signUpButton.layer.cornerRadius = 8;
     self.signUpButton.clipsToBounds = YES;
+}
+
+- (void)keyboardWillShowOrHide:(NSNotification *)notification
+{
+    [UIView animateWithDuration:[notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue]
+                          delay:0
+                        options:[notification.userInfo[UIKeyboardAnimationCurveUserInfoKey] unsignedIntegerValue] animations:^{
+                            CGFloat height = 0;
+                            if ([notification.name isEqualToString:UIKeyboardWillShowNotification]) {
+                                height = -1 * [notification.userInfo[UIKeyboardFrameBeginUserInfoKey] CGRectValue].size.height;
+                            }
+                            
+                            self.topVerticalSpaceConstraint.constant = height;
+                            self.bottomVerticalSpaceConstraint.constant = -1 * height;
+                            [self.view layoutIfNeeded];
+                        } completion:nil];
 }
 
 /*
