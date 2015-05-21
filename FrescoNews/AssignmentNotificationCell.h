8beaf1aa97b4eb8b74560ef2aecd6a2feefd2a9b
Jason Gresh <jrgresh@gmail.com>
2015-05-21 18:15:56 -0400

    Added AssignmentNotificationCell for the notification view controller

---
+++ FrescoNews/AssignmentNotificationCell.h
@@ -0,0 +1,13 @@
+//
+//  AssignmentNotificationCell.h
+//  FrescoNews
+//
+//  Created by Jason Gresh on 5/21/15.
+//  Copyright (c) 2015 Fresco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AssignmentNotificationCell : UITableViewCell
+
+@end
---
+++ FrescoNews/AssignmentNotificationCell.m
@@ -0,0 +1,13 @@
+//
+//  AssignmentNotificationCell.m
+//  FrescoNews
+//
+//  Created by Jason Gresh on 5/21/15.
+//  Copyright (c) 2015 Fresco. All rights reserved.
+//
+
+#import "AssignmentNotificationCell.h"
+
+@implementation AssignmentNotificationCell
+
+@end
