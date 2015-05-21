a11b7fbd21534f9d28aa2543d8113edbefb5ad1b
Zachary Mayberry <zachmayberry@gmail.com>
2015-05-21 11:42:50 -0500

    Initial commit with new tableview

---
+++ FrescoNews/NotificationsViewController.m
@@ -0,0 +1,84 @@
+//
+//  NotificationsViewController.m
+//  FrescoNews
+//
+//  Created by Zachary Mayberry on 5/21/15.
+//  Copyright (c) 2015 Fresco. All rights reserved.
+//
+
+#import "NotificationsViewController.h"
+
+@interface NotificationsViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation NotificationsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    self.tableView.estimatedRowHeight = 96;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // since there is a section for every story
+    // and just one story per section
+    // the section will tell us the "row"
+    NSUInteger index = indexPath.section;
+    
+    UITableViewCell *tableViewCell = [[UITableViewCell alloc] init];
+    
+    return tableViewCell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 40;
+}
+
+-(UIView *) tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+     UITableViewCell *tableViewCellHeader = [[UITableViewCell alloc] init];
+    
+    return tableViewCellHeader;
+}
+
+
+@end
