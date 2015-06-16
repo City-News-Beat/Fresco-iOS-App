0eb24046947709ad44140d939ef10e12e65c2665
Elmir Kouliev <elmir@kouliev.com>
2015-06-15 21:02:46 -0400

    Update run

--- FrescoNews/FirstRunViewController.m
+++ FrescoNews/FirstRunViewController.m
@@ -117,32 +117,7 @@
 
 - (IBAction)signUpButtonAction:(id)sender
 {
-    if ([self.emailField.text length] == 0 || [self.passwordField.text length] == 0) {
-        [self performSegueWithIdentifier:@"showInitialSignUp" sender:self];
-        return;
-    }
-    else {
-    
-        [[FRSDataManager sharedManager] signupUser:self.emailField.text
-                                              email:self.emailField.text
-                                           password:self.passwordField.text
-                                              block:^(BOOL succeeded, NSError *error) {
-                                                  if (!error)
-                                                      [self performSegueWithIdentifier:@"showSignUp" sender:self];
-                                                  else {
-                                                      UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
-                                                                                                      message:[error.userInfo objectForKey:@"error"]
-                                                                                                     delegate: self
-                                                                                            cancelButtonTitle: @"Cancel"
-                                                                                            otherButtonTitles:nil, nil];
-                                                      [alert addButtonWithTitle:@"Try Again"];
-                                                      [alert show];
-                                                      
-                                                      self.emailField.textColor = [UIColor redColor];
-                                                  }
-                                                  
-                                              }];
-    }
+    [self performSegueWithIdentifier:@"showInitialSignUp" sender:self];
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
