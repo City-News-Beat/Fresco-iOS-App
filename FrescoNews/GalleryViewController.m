319d4ac8e184fb251d2af3b5dffe272b3ff4b428
Joshua C. Lerner <jlerner@gmail.com>
2015-06-26 15:18:24 -0400

    "Add location to gallery banner" (https://trello.com/c/35ONqce5/143-add-location-to-gallery-banner)

--- FrescoNews/GalleryViewController.m
+++ FrescoNews/GalleryViewController.m
@@ -18,37 +18,21 @@
 #import "StoryViewController.h"
 
 @interface GalleryViewController ()  <UITableViewDataSource, UITableViewDelegate>
-
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-
 @property (weak, nonatomic) IBOutlet GalleryView *galleryView;
-
 @property (weak, nonatomic) IBOutlet UILabel *timeAndPlace;
-
 @property (weak, nonatomic) IBOutlet UILabel *byline;
-
 @property (weak, nonatomic) IBOutlet UILabel *caption;
-
 @property (weak, nonatomic) IBOutlet UIView *storiesView;
-
 @property (weak, nonatomic) IBOutlet UIView *articlesView;
-
 @property (weak, nonatomic) IBOutlet UILabel *storiesLabel;
-
 @property (weak, nonatomic) IBOutlet UITableView *storiesTable;
-
 @property (weak, nonatomic) IBOutlet UILabel *articlesTitle;
-
 @property (weak, nonatomic) IBOutlet UITableView *articlesTable;
-
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintArticleTableHeight;
-
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintStoriesTableHeight;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintStoriesDiff;
-
-
 @property (nonatomic, assign) BOOL bordersLaidOut;
-
 @end
 
 @implementation GalleryViewController
@@ -56,26 +40,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
     UIBarButtonItem *shareIcon = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(shareGallery:)];
-    
-    
     [self.navigationItem setRightBarButtonItem:shareIcon];
-    
-
     [self setUpGallery];
-    
 }
 
-
--(void)viewDidLayoutSubviews
+- (void)viewDidLayoutSubviews
 {
     [super viewDidLayoutSubviews];
-    
-    //
     self.constraintArticleTableHeight.constant = self.articlesTable.contentSize.height;
     self.constraintStoriesTableHeight.constant = self.storiesTable.contentSize.height;
-    
 
     //Redraw Table Views
     [self.articlesTable.layer layoutIfNeeded];
@@ -85,14 +59,13 @@
     [self.storiesTable.layer layoutIfNeeded];
     [self.storiesTable setNeedsLayout];
     [self.storiesTable layoutIfNeeded];
-    
     [self.scrollView setNeedsLayout];
     [self.scrollView layoutIfNeeded];
     
-    if(!self.bordersLaidOut){
+    if (!self.bordersLaidOut) {
     
         /* Borders */
-        if(self.gallery.articles.count != 0){
+        if (self.gallery.articles.count != 0) {
             CALayer *topLayerArticles = [CALayer layer];
             topLayerArticles.frame = CGRectMake(0.0f, 0.0f, self.articlesTable.frame.size.width, 1.0f);
             topLayerArticles.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:.12].CGColor;
@@ -104,8 +77,8 @@
             [self.articlesTable.layer addSublayer:topLayerArticles];
             [self.articlesTable.layer addSublayer:bottomLayerArticles];
         }
-        if(self.gallery.relatedStories.count != 0){
-            
+
+        if (self.gallery.relatedStories.count != 0) {
             CALayer *topLayerStories = [CALayer layer];
             topLayerStories.frame = CGRectMake(0.0f, 0.0f, self.storiesTable.frame.size.width, 1.0f);
             topLayerStories.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:.12].CGColor;
@@ -117,92 +90,70 @@
             [self.storiesTable.layer addSublayer:bottomLayerStories];
             [self.storiesTable.layer addSublayer:topLayerStories];
         }
-        
     }
     
     self.bordersLaidOut = YES;
-    
-
 }
 
--(void)shareGallery:(id)sender{
-    
+- (void)shareGallery:(id)sender
+{
     NSString *string = [NSString stringWithFormat:@"http://fresconews.com/gallery/%@", self.gallery.galleryID];
     NSURL *URL = [NSURL URLWithString:string];
     
-    UIActivityViewController *activityViewController =
-    [[UIActivityViewController alloc] initWithActivityItems:@[string, URL]
-                                      applicationActivities:nil];
+    UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:@[string, URL]
+                                                                                         applicationActivities:nil];
     [self.navigationController presentViewController:activityViewController
-                                       animated:YES
-                                     completion:^{
-                                         // ...
-                                     }];
-
+                                            animated:YES
+                                          completion:^{
+                                              // ...
+                                          }];
 }
 
-
-- (void)setUpGallery{
-    
+- (void)setUpGallery
+{
     self.articlesTable.separatorStyle = UITableViewCellSeparatorStyleNone;
     self.storiesTable.separatorStyle = UITableViewCellSeparatorStyleNone;
-    
     self.galleryView.gallery = self.gallery;
-    
     self.caption.text = self.gallery.caption;
-    
-    self.timeAndPlace.text = [MTLModel relativeDateStringFromDate:self.gallery.createTime];
-    
+    self.timeAndPlace.text = [NSString stringWithFormat:@"%@, %@", ((FRSPost *)[self.gallery.posts firstObject]).address,
+                                                                   [MTLModel relativeDateStringFromDate:self.gallery.createTime]];
+
     self.byline.text = ((FRSPost *)[self.gallery.posts firstObject]).byline;
 
-    if(self.gallery.articles.count == 0){
-
+    if (self.gallery.articles.count == 0) {
         self.articlesView.hidden = YES;
-    
         [self.view addConstraints:
-         [NSLayoutConstraint constraintsWithVisualFormat:@"V:[articlesView(0)]"
+        [NSLayoutConstraint constraintsWithVisualFormat:@"V:[articlesView(0)]"
                                                  options:0
                                                  metrics:nil
                                                    views: @{@"articlesView":self.articlesView}]];
 
     }
-    if(self.gallery.relatedStories.count == 0){
-    
+
+    if (self.gallery.relatedStories.count == 0) {
         self.storiesView.hidden = YES;
-    
         self.constraintStoriesDiff.constant = 0;
+        [self.view addConstraints: [NSLayoutConstraint constraintsWithVisualFormat:@"V:[storiesView(0)]"
+                                                                           options:0
+                                                                           metrics:nil
+                                                                             views: @{@"storiesView":self.storiesView}]];
 
-        [self.view addConstraints:
-         [NSLayoutConstraint constraintsWithVisualFormat:@"V:[storiesView(0)]"
-                                                 options:0
-                                                 metrics:nil
-                                                   views: @{@"storiesView":self.storiesView}]];
-    
-        [self.storiesView addConstraints:
-         [NSLayoutConstraint constraintsWithVisualFormat:@"V:[storiesView(0)]"
-                                                 options:0
-                                                 metrics:nil
-                                                   views: @{@"storiesView":self.storiesTable}]];
+        [self.storiesView addConstraints: [NSLayoutConstraint constraintsWithVisualFormat:@"V:[storiesView(0)]"
+                                                                                  options:0
+                                                                                  metrics:nil
+                                                                                    views: @{@"storiesView":self.storiesTable}]];
     }
-    
 }
 
-- (void)openGalleryWithId:(NSString *)galleryId{
-
+- (void)openGalleryWithId:(NSString *)galleryId
+{
     [[FRSDataManager sharedManager] getGallery:galleryId WithResponseBlock:^(id responseObject, NSError *error) {
-        
         if (!error) {
-            
             [self setGallery:responseObject];
-            
             [self setUpGallery];
-            
         }
-        
     }];
-
 }
-
 
 #pragma mark - UITableViewDataSource
 
@@ -213,26 +164,20 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    if(tableView == self.storiesTable){
+    if (tableView == self.storiesTable) {
         return self.gallery.relatedStories.count;
     }
-    else if(tableView == self.articlesTable){
-        
+    else if (tableView == self.articlesTable) {
         return self.gallery.articles.count;
-        
     }
     
     return 0;
-    
 }
 
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
-    if(tableView == self.storiesTable){
-        
+    if (tableView == self.storiesTable) {
         UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"relatedStoryCell"];
-        
         UILabel *storyTitle = (UILabel *)[cell viewWithTag:100];
         
         [storyTitle setText:[[[self gallery] relatedStories] objectAtIndex:indexPath.row][@"title"]];
@@ -244,84 +189,57 @@
         }
         
         return cell;
-        
     }
-    else if(tableView == self.articlesTable){
-        
+    else if (tableView == self.articlesTable) {
         FRSArticle *article = [[[self gallery] articles] objectAtIndex:indexPath.row];
-        
         UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"articleCell"];
-        
         UILabel *articleTitle = (UILabel *)[cell viewWithTag:100];
-        
         UIImageView *articleImage = (UIImageView *)[cell viewWithTag:200];
-        
         articleTitle.text = article.title;
-        
         [articleImage setImageWithURL:article.URL];
-        
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
         
         if (indexPath.row != self.gallery.articles.count -1) {
-            UIView* separatorLineView = [[UIView alloc] initWithFrame:CGRectMake(0, 43, 320, 1)];/// change size as you need.
-            separatorLineView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:.12];// you can also put image here
+            UIView* separatorLineView = [[UIView alloc] initWithFrame:CGRectMake(0, 43, 320, 1)]; /// change size as you need.
+            separatorLineView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:.12]; // you can also put image here
             [cell.contentView addSubview:separatorLineView];
         }
         
         return cell;
-        
     }
     
-    return 0;
-    
+    return nil;
 }
 
 #pragma mark - UITableViewDelegate
 
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    
-    if(tableView == self.storiesTable){
-        
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (tableView == self.storiesTable) {
         NSString *storyId = [[[self gallery] relatedStories] objectAtIndex:indexPath.row][@"_id"];
-        
         [[FRSDataManager sharedManager] getStory:storyId withResponseBlock:^(id responseObject, NSError *error) {
-            
             if (!error) {
-                
                 StoryViewController *storyViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"storyViewController"];
-                
                 storyViewController.story = responseObject;
-                
                 [self.navigationController pushViewController:storyViewController animated:YES];
-                
             }
-            
         }];
-        
-        
-
     }
-    else if(tableView == self.articlesTable){
-        
+    else if (tableView == self.articlesTable) {
         FRSArticle *article = [[[self gallery] articles] objectAtIndex:indexPath.row];
-        
         STKWebKitViewController *controller = [[STKWebKitViewController alloc] initWithURL:article.URL];
-        
         [self.navigationController pushViewController:controller animated:YES];
-
     }
-
 }
 
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
     return 44.0f;
 }
 
-- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
     return 0.0f;
 }
-
-
 
 @end
