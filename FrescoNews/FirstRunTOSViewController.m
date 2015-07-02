634cb7e88b83b24930131e0be59f76454f67ca79
Zachary Mayberry <zachmayberry@gmail.com>
2015-07-02 16:45:55 -0500

    Create outlet for textview and disable accept button until bottom is reached

--- FrescoNews/FirstRunTOSViewController.m
+++ FrescoNews/FirstRunTOSViewController.m
@@ -8,7 +8,10 @@
 
 #import "FirstRunTOSViewController.h"
 
-@interface FirstRunTOSViewController ()
+@interface FirstRunTOSViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIButton *agreeButton;
+@property (weak, nonatomic) IBOutlet UITextView *tosTextView;
 @end
 
 @implementation FirstRunTOSViewController
@@ -17,11 +20,26 @@
 {
     [super viewDidLoad];
     // Call endpoint
+    
+    self.scrollView.delegate = self;
+    
+    self.agreeButton.enabled = NO;
+    
+    
 }
 
 - (IBAction)actionDone:(id)sender
 {
     [self navigateToMainApp];
 }
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    float bottomEdge = scrollView.contentOffset.y + scrollView.frame.size.height;
+    if (bottomEdge >= scrollView.contentSize.height) {
+        // we are at the end
+        self.agreeButton.enabled = YES;
+    }
+}
+
 
 @end
