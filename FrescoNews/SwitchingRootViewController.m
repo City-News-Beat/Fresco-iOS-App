b61619ce6076315a95ca5c6154900fa5bcb35f38
Joshua C. Lerner <jlerner@gmail.com>
2015-07-03 14:19:40 -0400

    Scroll to top when user taps current tab (0, 1, or 4)
    A bit cleaner implementation

--- FrescoNews/SwitchingRootViewController.m
+++ FrescoNews/SwitchingRootViewController.m
@@ -9,7 +9,10 @@
 #import "SwitchingRootViewController.h"
 @import AVFoundation;
 #import "TabBarController.h"
+#import "HomeViewController.h"
+#import "StoriesViewController.h"
 #import "CameraViewController.h"
+#import "ProfileViewController.h"
 
 @interface SwitchingRootViewController () <UITabBarControllerDelegate, UIAlertViewDelegate>
 @property (weak, nonatomic) IBOutlet UIView *viewContainer;
@@ -144,7 +147,16 @@
 
 - (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController
 {
-    if ([viewController isMemberOfClass:[UIViewController class]]) {
+    UIViewController *vc = viewController.childViewControllers[0];
+    if ([vc isMemberOfClass:[HomeViewController class]] && tabBarController.selectedIndex == 0) {
+        [((HomeViewController *)vc).galleriesViewController.tableView setContentOffset:CGPointZero animated:YES];
+        return NO;
+    }
+    else if ([vc isMemberOfClass:[StoriesViewController class]] && tabBarController.selectedIndex == 1) {
+        [((StoriesViewController *)vc).tableView setContentOffset:CGPointZero animated:YES];
+        return NO;
+    }
+    else if ([viewController isMemberOfClass:[UIViewController class]]) {
         if ([AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo] == AVAuthorizationStatusDenied) {
             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Enable Camera"
                                                             message:@"Fresco needs permission to access the camera to continue."
@@ -156,9 +168,12 @@
 
         return NO;
     }
-    else {
-        return YES;
+    else if ([vc isMemberOfClass:[ProfileViewController class]] && tabBarController.selectedIndex == 4) {
+        [((ProfileViewController *)vc).galleriesViewController.tableView setContentOffset:CGPointZero animated:YES];
+        return NO;
     }
+
+    return YES;
 }
 
 #pragma mark - UIAlertViewDelegate methods
