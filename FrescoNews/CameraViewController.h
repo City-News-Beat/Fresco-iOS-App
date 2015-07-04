City-News-Beat/Fresco-iOS-App

commit b61619ce6076315a95ca5c6154900fa5bcb35f38

Scroll to top when user taps current tab (0, 1, or 4)
A bit cleaner implementation

FrescoNews/SwitchingRootViewController.m
@@ -9,7 +9,10 @@
 #import "SwitchingRootViewController.h"
 @import AVFoundation;
 #import "TabBarController.h"
+#import "HomeViewController.h"
+#import "StoriesViewController.h"
 #import "CameraViewController.h"
+#import "ProfileViewController.h"
 
 @interface SwitchingRootViewController () <UITabBarControllerDelegate, UIAlertViewDelegate>
 @property (weak, nonatomic) IBOutlet UIView *viewContainer;
@@ -144,7 +147,16 @@
 
 - (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController
 {
-    if ([viewController isMemberOfClass:[UIViewController class]]) {
+    UIViewController *vc = viewController.childViewControllers[0];
+    if ([vc isMemberOfClass:[HomeViewController class]] && tabBarController.selectedIndex == 0) {
+        [((HomeViewController *)vc).galleriesViewController.tableView setContentOffset:CGPointZero animated:YES];
+        return NO;
+    }
+    else if ([vc isMemberOfClass:[StoriesViewController class]] && tabBarController.selectedIndex == 1) {
+        [((StoriesViewController *)vc).tableView setContentOffset:CGPointZero animated:YES];
+        return NO;
+    }
+    else if ([viewController isMemberOfClass:[UIViewController class]]) {
         if ([AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo] == AVAuthorizationStatusDenied) {
             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Enable Camera"
                                                             message:@"Fresco needs permission to access the camera to continue."
@@ -156,9 +168,12 @@
 
         return NO;
     }
-    else {
-        return YES;
+    else if ([vc isMemberOfClass:[ProfileViewController class]] && tabBarController.selectedIndex == 4) {
+        [((ProfileViewController *)vc).galleriesViewController.tableView setContentOffset:CGPointZero animated:YES];
+        return NO;
     }
+
+    return YES;
 }
 
 #pragma mark - UIAlertViewDelegate methods

FrescoNews/TabBarController.m
@@ -20,51 +20,6 @@
 
 - (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item
 {
-
-    UITabBarController *tabBarController = ((UITabBarController *)((SwitchingRootViewController *)[UIApplication sharedApplication].keyWindow.rootViewController).viewController);
-    
-    if ([item.title isEqualToString:@"Highlights"]) {
-    
-        HomeViewController *vc = (HomeViewController *) ([[tabBarController viewControllers][0] viewControllers][0]);
-        
-        [vc.galleriesViewController.tableView setContentOffset:CGPointZero animated:YES];
-        
-        [tabBarController setSelectedIndex:0];
-    
-    }
-    else if ([item.title isEqualToString:@"Stories"]) {
-        
-        StoriesViewController *vc = (StoriesViewController *) ([[tabBarController viewControllers][1] viewControllers][0]);
-        
-        [vc.tableView setContentOffset:CGPointZero animated:YES];
-        
-        [tabBarController setSelectedIndex:1];
-    
-        
-    }
-    else if ([item.title isEqualToString:@"Assignments"]) {
-        
-//        AssignmentsViewController *vc = (AssignmentsViewController *) ([[tabBarController viewControllers][3] viewControllers][0]);
-//        
-//        vc.centeredUserLocation = NO;
-//        
-//        [vc zoomToCurrentLocation];
-//        
-//        [tabBarController setSelectedIndex:3];
-
-    }
-    else if ([item.title isEqualToString:@"Profile"]) {
-        
-        ProfileViewController *vc = (ProfileViewController *) ([[tabBarController viewControllers][4] viewControllers][0]);
-        
-        [vc.galleriesViewController.tableView setContentOffset:CGPointZero animated:YES];
-        
-        [tabBarController setSelectedIndex:4];
-        
-    }
-    
-
-    
     [[NSUserDefaults standardUserDefaults] setInteger:self.selectedIndex forKey:@"previouslySelectedTab"];
     if ([item.title isEqualToString:@"Camera"]) {
         if ([AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo] != AVAuthorizationStatusDenied) {

commit e728d679863441d5b1b1368f6f71c5ab4bffeb5f

Fix app crash when selecting camera tab
Bug introduced in b61619c.

FrescoNews/CameraViewController.h
@@ -15,3 +15,7 @@
 - (IBAction)doneButtonTapped:(id)sender;
 
 @end
+
+@interface TemplateCameraViewController : UIViewController
+// Do not delete
+@end

FrescoNews/CameraViewController.m
@@ -19,6 +19,10 @@
 #import "SwitchingRootViewController.h"
 #import "MKMapView+Additions.h"
 
+@implementation TemplateCameraViewController
+// Do not delete
+@end
+
 typedef enum : NSUInteger {
     CameraModePhoto,
     CameraModeVideo

FrescoNews/SwitchingRootViewController.m
@@ -147,16 +147,7 @@
 
 - (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController
 {
-    UIViewController *vc = viewController.childViewControllers[0];
-    if ([vc isMemberOfClass:[HomeViewController class]] && tabBarController.selectedIndex == 0) {
-        [((HomeViewController *)vc).galleriesViewController.tableView setContentOffset:CGPointZero animated:YES];
-        return NO;
-    }
-    else if ([vc isMemberOfClass:[StoriesViewController class]] && tabBarController.selectedIndex == 1) {
-        [((StoriesViewController *)vc).tableView setContentOffset:CGPointZero animated:YES];
-        return NO;
-    }
-    else if ([viewController isMemberOfClass:[UIViewController class]]) {
+    if ([viewController isMemberOfClass:[TemplateCameraViewController class]]) {
         if ([AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo] == AVAuthorizationStatusDenied) {
             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Enable Camera"
                                                             message:@"Fresco needs permission to access the camera to continue."
@@ -168,9 +159,20 @@
 
         return NO;
     }
-    else if ([vc isMemberOfClass:[ProfileViewController class]] && tabBarController.selectedIndex == 4) {
-        [((ProfileViewController *)vc).galleriesViewController.tableView setContentOffset:CGPointZero animated:YES];
-        return NO;
+    else {
+        UIViewController *vc = [viewController.childViewControllers firstObject];
+        if ([vc isMemberOfClass:[HomeViewController class]] && tabBarController.selectedIndex == 0) {
+            [((HomeViewController *)vc).galleriesViewController.tableView setContentOffset:CGPointZero animated:YES];
+            return NO;
+        }
+        else if ([vc isMemberOfClass:[StoriesViewController class]] && tabBarController.selectedIndex == 1) {
+            [((StoriesViewController *)vc).tableView setContentOffset:CGPointZero animated:YES];
+            return NO;
+        }
+        else if ([vc isMemberOfClass:[ProfileViewController class]] && tabBarController.selectedIndex == 4) {
+            [((ProfileViewController *)vc).galleriesViewController.tableView setContentOffset:CGPointZero animated:YES];
+            return NO;
+        }
     }
 
     return YES;
@@ -184,8 +186,5 @@
         [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
     }
 }
-
-
-
 
 @end