3df4e7aae3528ad667eef9e47a5a5a9f5bb78fb6
Nick <nrizk6@gmail.com>
2015-08-20 15:29:10 -0400

    add loading indicator to photo collection view cells

--- Fresco News/Views/PostCollectionViewCell.m
+++ Fresco News/Views/PostCollectionViewCell.m
@@ -71,50 +71,74 @@ static NSString * const kCellIdentifier = @"PostCollectionViewCell";
             self.videoIndicatorView.alpha = 0;
             self.videoIndicatorView.hidden = YES;
             
+           
             //Add subviews and bring to the front so they don't get hidden
             [self addSubview:self.playPause];
             [self addSubview:self.videoIndicatorView];
             [self bringSubviewToFront:self.videoIndicatorView];
             [self bringSubviewToFront:self.playPause];
             
-        }
+        } else {
+            
+            self.photoIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+            self.photoIndicatorView.frame = CGRectMake(self.frame.size.width/2, self.frame.size.height/2, 0, 0);
+            [self addSubview:self.photoIndicatorView];
+            [self bringSubviewToFront:self.photoIndicatorView];
         
-        [self.imageView setImageWithURLRequest:[NSURLRequest requestWithURL:[self.post largeImageURL]] placeholderImage:nil success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
+        }
+       
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
             
-            self.processingVideo = false;
-            
-            weakSelf.imageView.image = image;
-            
-            weakSelf.imageView.alpha = 1.0f;
-            
-            [weakSelf removeTranscodePlaceHolder];
-            
-        } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
-            
-            if([self.post isVideo]){
+            //back to the main thread for the UI call
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.photoIndicatorView startAnimating];
+            });
+            // more on the background thread
+            [self.imageView setImageWithURLRequest:[NSURLRequest requestWithURL:[self.post largeImageURL]] placeholderImage:nil success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
                 
-                if(response != nil){
+                self.processingVideo = false;
                 
-                    self.processingVideo = true;
+                weakSelf.imageView.image = image;
+                
+                weakSelf.imageView.alpha = 1.0f;
+                
+                [weakSelf removeTranscodePlaceHolder];
+                
+                //back to the main thread for the UI call
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self.photoIndicatorView stopAnimating];
+                });
+                
+            } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
+                
+                if([self.post isVideo]){
                     
-                    self.transcodeImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"transcoding"]];
-                    self.transcodeImage.frame = CGRectMake(0, 0, 150, 150);
-                    self.transcodeImage.center = self.center;
-                
-                    self.transcodeLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, 80)];
-                    self.transcodeLabel.text = @"We’re still processing this video!";
-                    self.transcodeLabel.font= [UIFont fontWithName:HELVETICA_NEUE_LIGHT size:18.0f];
-                    self.transcodeLabel.center = CGPointMake(self.center.x, self.center.y + 100);
-                    self.transcodeLabel.textAlignment = NSTextAlignmentCenter;
-                    
-                    [self addSubview:self.transcodeImage];
-                    [self addSubview:self.transcodeLabel];
+                    if(response != nil){
+                        
+                        self.processingVideo = true;
+                        
+                        self.transcodeImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"transcoding"]];
+                        self.transcodeImage.frame = CGRectMake(0, 0, 150, 150);
+                        self.transcodeImage.center = self.center;
+                        
+                        self.transcodeLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, 80)];
+                        self.transcodeLabel.text = @"We’re still processing this video!";
+                        self.transcodeLabel.font= [UIFont fontWithName:HELVETICA_NEUE_LIGHT size:18.0f];
+                        self.transcodeLabel.center = CGPointMake(self.center.x, self.center.y + 100);
+                        self.transcodeLabel.textAlignment = NSTextAlignmentCenter;
+                        
+                        [self addSubview:self.transcodeImage];
+                        [self addSubview:self.transcodeLabel];
+                        
+                    }
                     
                 }
                 
-            }
+            }];
             
-        }];
+        });
+        
+
     }
     else {
         // local
