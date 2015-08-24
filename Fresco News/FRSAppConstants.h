63fbf2421d981e0dd03f0cec68cb7a8979bdddaf
Nick <nrizk6@gmail.com>
2015-08-24 11:12:25 -0400

    organize user-facing string macros

--- Fresco News/FRSAppConstants.h
+++ Fresco News/FRSAppConstants.h
@@ -10,6 +10,9 @@
 #import "UIColor+Additions.h"
 #import "FRSAlertViewManager.h"
 
+
+#pragma mark - Enums
+
 enum FRSErrorCodes {
     ErrorSignupDuplicateEmail = 101,
     ErrorSignupCantCreateUser,
@@ -19,12 +22,16 @@ enum FRSErrorCodes {
     ErrorSignupNoUserFromParseUser,
 } frsErrorCodes;
 
+
+
+#pragma mark - Notification Strings
+
 #define NOTIFICATION_API_KEY_AVAILABLE      @"NotificationAPIKeyAvailable"
 #define NOTIFICATION_VIEW_DISMISS           @"DismissNotificationsView"
 #define NOTIFICATION_BADGE_RESET            @"ResetNotificationBadge"
 #define NOTIFICATION_IMAGE_SET              @"UserProfileImageChanged"
 
-#define ERROR_DOMAIN                        @"com.fresconews"
+
 
 
 #pragma mark - Keys Plist
@@ -33,10 +40,13 @@ enum FRSErrorCodes {
 #define KEYS_DICTIONARY                     [NSDictionary dictionaryWithContentsOfFile:KEYS_PLIST_PATH]
 
 
+
+
 #pragma mark - Base URL/API & Parse
 
-#define BASE_PATH                           @""
+#define ERROR_DOMAIN                        @"com.fresconews"
 
+#define BASE_PATH                           @""
 
 #ifdef DEBUG
     #define BASE_URL                        @"https://alpha.fresconews.com"
@@ -51,6 +61,8 @@ enum FRSErrorCodes {
 #endif
 
 
+
+
 #pragma mark - Twitter Auth
 
 #define TWITTER_CONSUMER_KEY                [KEYS_DICTIONARY objectForKey:@"TwitterConsumerKey"]
@@ -59,15 +71,22 @@ enum FRSErrorCodes {
 #define TWITTER_VERIFY_URL                  @"https://api.twitter.com/1.1/account/verify_credentials.json"
 
 
+
+
 #pragma mark - CDN
 
 ///
+
+
+
 
 #pragma mark - Float/Int Values
 
 #define MAX_VIDEO_LENGTH                    60.0f
 #define MAX_ASSET_AGE                       -3600 * 6
 #define LOCATION_UPDATE_INTERVAL            60
+
+
 
 
 #pragma mark - User Defaults
@@ -77,7 +96,6 @@ enum FRSErrorCodes {
 #define UD_AVATAR                           @"avatar"
 #define UD_TOKEN                            @"frescoAPIToken"
 #define UD_TOS_AGREED                       @"tos"
-
 #define UD_CAPTION_STRING_IN_PROGRESS       @"captionStringInProgress"
 #define UD_DEFAULT_ASSIGNMENT_ID            @"defaultAssignmentID"
 #define UD_SELECTED_ASSETS                  @"selectedAssets"
@@ -89,6 +107,9 @@ enum FRSErrorCodes {
 #define UD_UPDATE_PROFILE                   @"updateProfile"
 #define UD_UPDATE_USER_GALLERIES            @"updateUserGalleries"
 
+
+
+
 #pragma mark - Fonts
 
 #define HELVETICA_NEUE_MEDIUM               @"HelveticaNeue-Medium"
@@ -97,11 +118,15 @@ enum FRSErrorCodes {
 #define HELVETICA_NEUE_REGULAR              @"HelveticaNeue"
 
 
+
+
 #pragma mark - MapView Identifiers
 
 #define ASSIGNMENT_IDENTIFIER               @"AssignmentAnnotation"
 #define CLUSTER_IDENTIFIER                  @"ClusterAnnotation"
 #define USER_IDENTIFIER                     @"currentLocation"
+
+
 
 
 #pragma mark - Segue Identifiers
@@ -114,20 +139,34 @@ enum FRSErrorCodes {
 #define SEG_SHOW_RADIUS                     @"showRadius"
 #define SEG_SETTINGS                        @"settingsSegue"
 
+
+
+
 #pragma mark - Notification Categories/Actions
 
 #define ASSIGNMENT_CATEGORY                 @"ASSIGNMENT_CATEGORY"
 #define NAVIGATE_IDENTIFIER                 @"NAVIGATE_IDENTIFIER"
 
-#pragma mark - User-facing Strings
+
+
+
+
+#pragma mark - User-facing Strings -
+
+
+
+
+#pragma mark - Brand Names / Trademarks
 
 #define FRESCO                              @"Fresco" // Not localizing name i.e. would be Fresh in Spanish
 #define FACEBOOK                            @"Facebook"
 #define TWITTER                             @"Twitter"
-#define FB_LOGOUT_PROMPT                    NSLocalizedString(@"It seems like you logged in through Facebook. If you disconnect it, this would disable your account entirely!", nil)
 
-#define AWKWARD                             NSLocalizedString(@"AWKWARD", nil)
-#define NO_IPHONE_4S_LABEL                  NSLocalizedString(@"Fresco does not support your screen size yet.", nil)
+
+
+
+#pragma mark - Global Macros
+
 #define OK                                  NSLocalizedString(@"OK", nil)
 #define DISMISS                             NSLocalizedString(@"Dismiss", nil)
 #define DISABLE                             NSLocalizedString(@"Disable", nil)
@@ -140,36 +179,26 @@ enum FRSErrorCodes {
 #define WARNING                             NSLocalizedString(@"Warning", nil)
 #define WHATS_HAPPENING                     NSLocalizedString(@"What's happening?", nil)
 
+
+
+
+#pragma mark - First Run Log in / Sign up
+
 #define LOGIN                               NSLocalizedString(@"Login", nil)
 #define LOGIN_ERROR                         NSLocalizedString(@"Login Error", nil)
 #define LOGIN_PROMPT                        NSLocalizedString(@"Please enter a valid email and password", nil)
 #define NAME_PROMPT                         NSLocalizedString(@"Please enter both first and last name", nil)
 #define NAME_ERROR_MSG                      NSLocalizedString(@"We could not successfully save your first and last name", nil)
-#define AVATAR_PROMPT                       NSLocalizedString(@"Choose a new avatar", nil)
-#define NOTIF_RADIUS_ERROR_MSG              NSLocalizedString(@"Could not save notification radius", nil)
-#define T_O_S_UNAVAILABLE_MSG               NSLocalizedString(@"Terms of Service not available", nil)
-#define DISABLE_ACCT_TITLE                  NSLocalizedString(@"Are you sure? You can recover your account up to one year from today.", nil)
-
-#define PROFILE_SAVE_ERROR                  NSLocalizedString(@"Could not save Profile settings", nil)
-#define DISABLE_ACCT_ERROR                  NSLocalizedString(@"It seems we couldn't successfully disable your account. Please contact support@fresconews.com for help.", nil)
 #define PASSWORD_ERROR_TITLE                NSLocalizedString(@"Passwords do not match", nil)
 #define PASSWORD_ERROR_MESSAGE              NSLocalizedString(@"Please make sure your new passwords are equal", nil)
-
 #define INVALID_CREDENTIALS                 NSLocalizedString(@"Invalid Credentials", nil)
 #define TWITTER_ERROR                       NSLocalizedString(@"We ran into an error signing you in with Twitter", nil)
 #define FACEBOOK_ERROR                      NSLocalizedString(@"We ran into an error signing you in with Facebook", nil)
 
-#define ACCT_WILL_BE_DISABLED               NSLocalizedString(@"Account will be disabled", nil)
-
-#define WELL_MISS_YOU                       NSLocalizedString(@"We'll miss you!", nil)
-#define YOU_CAN_LOGIN_FOR_ONE_YR            NSLocalizedString(@"You can log in any time in the next year to restore your account.", nil)
 
 
-#define GO_TO_SETTINGS                      NSLocalizedString(@"Go to Settings", nil)
-#define NAVIGATE_STR                        NSLocalizedString(@"Navigate", nil)
-#define NAVIGATE_TO_ASSIGNMENT              NSLocalizedString(@"Navigate to the assignment", nil)
-#define ENABLE_CAMERA_TITLE                 NSLocalizedString(@"Enable Camera", nil)
-#define ENABLE_CAMERA_MSG                   NSLocalizedString(@"needs permission to access the camera to continue.", nil)
+
+#pragma mark - First Run Permissions
 
 #define CAMERA_ENABLED                      NSLocalizedString(@"Camera Enabled", nil)
 #define CAMERA_DISABLED                     NSLocalizedString(@"Camera Disabled", nil)
@@ -180,11 +209,51 @@ enum FRSErrorCodes {
 #define NOTIF_PENDING                       NSLocalizedString(@"Notifications Pending", nil)
 #define NOTIF_ENABLED                       NSLocalizedString(@"Notifications Enabled", nil)
 
-#define OPEN_IN_MAPS                        NSLocalizedString(@"Open in Maps", nil)
-#define OPEN_IN_GOOG_MAPS                   NSLocalizedString(@"Open in Google Maps", nil)
+#define ENABLE_CAMERA_TITLE                 NSLocalizedString(@"Enable Camera", nil)
+#define ENABLE_CAMERA_MSG                   NSLocalizedString(@"needs permission to access the camera to continue.", nil)
+
+#define GO_TO_SETTINGS                      NSLocalizedString(@"Go to Settings", nil)
+
+#pragma mark - First Run Radius
+
+#define NOTIF_RADIUS_ERROR_MSG              NSLocalizedString(@"Could not save notification radius", nil)
+
+#pragma mark - First Run TOS
+
+#define T_O_S_UNAVAILABLE_MSG               NSLocalizedString(@"Terms of Service not available", nil)
+
+
+
+
+#pragma mark - Profile Settings
+
+#define AVATAR_PROMPT                       NSLocalizedString(@"Choose a new avatar", nil)
+
+#define PROFILE_SAVE_ERROR                  NSLocalizedString(@"Could not save Profile settings", nil)
+
+#define DISABLE_ACCT_TITLE                  NSLocalizedString(@"Are you sure? You can recover your account up to one year from today.", nil)
+#define DISABLE_ACCT_ERROR                  NSLocalizedString(@"It seems we couldn't successfully disable your account. Please contact support@fresconews.com for help.", nil)
+#define ACCT_WILL_BE_DISABLED               NSLocalizedString(@"Account will be disabled", nil)
+
+#define WELL_MISS_YOU                       NSLocalizedString(@"We'll miss you!", nil)
+#define YOU_CAN_LOGIN_FOR_ONE_YR            NSLocalizedString(@"You can log in any time in the next year to restore your account.", nil)
+
+#define FB_LOGOUT_PROMPT                    NSLocalizedString(@"It seems like you logged in through Facebook. If you disconnect it, this would disable your account entirely!", nil)
 
 #define NOTHING_HERE_YET                    NSLocalizedString(@"Nothing here yet!", nil)
 #define OPEN_CAMERA                         NSLocalizedString(@"Open your camera to get started", nil)
+
+
+
+
+#pragma mark - Unused
+
+#define NAVIGATE_STR                        NSLocalizedString(@"Navigate", nil)
+#define NAVIGATE_TO_ASSIGNMENT              NSLocalizedString(@"Navigate to the assignment", nil)
+
+
+
+#pragma mark - Onboarding
 
 #define MAIN_HEADER_1                       NSLocalizedString(@"Find breaking news around you", nil)
 #define MAIN_HEADER_2                       NSLocalizedString(@"Submit your photos and videos", nil)
@@ -194,16 +263,40 @@ enum FRSErrorCodes {
 #define SUB_HEADER_2                        NSLocalizedString(@"Your media is visible not only to Fresco users, but to our news organization partners in need of visual coverage", nil)
 #define SUB_HEADER_3                        NSLocalizedString(@"We notify you when your photos and videos are used, and you'll get paid if you took them for an assignment", nil)
 
+
+
+
+#pragma mark - Highlights
+
 #define HIGHLIGHTS                          NSLocalizedString(@"Highlights", nil)
+
+
+
+
+#pragma mark - Stories
+
 #define STORIES                             NSLocalizedString(@"Stories", nil)
+
+
+
+
+#pragma mark - Notifications
 
 #define VIEW                                NSLocalizedString(@"View", nil)
 #define VIEW_ASSIGNMENT                     NSLocalizedString(@"View Assignment", nil)
+
 #define ASSIGNMENT_EXPIRED_TITLE            NSLocalizedString(@"Assignment Expired", nil)
 #define ASSIGNMENT_EXPIRED_MSG              NSLocalizedString(@"This assignment has expired already!", nil)
 
+#define OPEN_IN_MAPS                        NSLocalizedString(@"Open in Maps", nil)
+#define OPEN_IN_GOOG_MAPS                   NSLocalizedString(@"Open in Google Maps", nil)
+
 #define GALLERY_UNAVAILABLE_TITLE           NSLocalizedString(@"Gallery Unavailable", nil)
 #define GALLERY_UNAVAILABLE_MSG             NSLocalizedString(@"We couldn't find this gallery!", nil)
+
+
+
+
 
 
 #pragma mark - Device Macros
