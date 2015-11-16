653c802c00103ed0b8b76c61f7c7428725ff751b
Daniel Sun <daniel.wm.sun@gmail.com>
2015-11-16 18:22:45 -0500

    scaffolding FRSAVSessionManager

---
+++ Fresco News/Singletons/FRSAVSessionManager.h
@@ -0,0 +1,18 @@
+//
+//  FRSAVSessionManager.h
+//  Fresco
+//
+//  Created by Daniel Sun on 11/16/15.
+//  Copyright © 2015 Fresco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FRSAVSessionManager : NSObject
+
++(instancetype)defaultManager;
+
+
+
+
+@end
