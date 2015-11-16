b35ce5290b80d816ab014fa929dce47f56eeab9c
Daniel Sun <daniel.wm.sun@gmail.com>
2015-11-16 13:37:29 -0500

    new design camera added

---
+++ Fresco News/View Controllers/Camera/FRSCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  FRSCameraViewController.h
+//  Fresco
+//
+//  Created by Daniel Sun on 11/13/15.
+//  Copyright © 2015 Fresco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRSCameraViewController : UIViewController
+
+@end
