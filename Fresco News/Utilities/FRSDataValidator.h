9a13eb0efda927490b90d3fafe1acb516354c277
Daniel Sun <daniel.wm.sun@gmail.com>
2015-12-21 15:28:04 -0500

    data valdiation and login start

---
+++ Fresco News/Utilities/FRSDataValidator.h
@@ -0,0 +1,13 @@
+//
+//  FRSDataValidator.h
+//  Fresco
+//
+//  Created by Daniel Sun on 12/21/15.
+//  Copyright © 2015 Fresco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FRSDataValidator : NSObject
+
+@end
---
+++ Fresco News/Utilities/FRSDataValidator.m
@@ -0,0 +1,13 @@
+//
+//  FRSDataValidator.m
+//  Fresco
+//
+//  Created by Daniel Sun on 12/21/15.
+//  Copyright © 2015 Fresco. All rights reserved.
+//
+
+#import "FRSDataValidator.h"
+
+@implementation FRSDataValidator
+
+@end
