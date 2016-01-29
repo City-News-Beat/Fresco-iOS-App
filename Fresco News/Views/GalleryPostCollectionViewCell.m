0db8cc5ab6dad8a1de70310f03c3a66003bb9960
Omar <omar.elfanek@gmail.com>
2016-01-29 13:16:57 -0500

    checking rgb values on image when uploading

--- Fresco News/Views/GalleryPostCollectionViewCell.m
+++ Fresco News/Views/GalleryPostCollectionViewCell.m
@@ -24,6 +24,8 @@ static NSString * const kCellIdentifier = @"PostCollectionViewCell";
 
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintImageViewAspectRatio;
 
+@property (strong, nonatomic) UIColor *color;
+
 @end
 
 @implementation GalleryPostCollectionViewCell
@@ -87,25 +89,84 @@ static NSString * const kCellIdentifier = @"PostCollectionViewCell";
     [self configureTranscodingImage];
 }
 
-- (void)configureTranscodingImage{
+- (void)configureTranscodingImage {
+
+    [self GetCurrentPixelColorAtPoint:CGPointMake(self.imageView.center.x, self.imageView.center.y)];
     
-    //Look over imogens notes in slack
-    //Need asset rendered in white
+
+    const CGFloat* components = CGColorGetComponents(self.color.CGColor);
+    NSLog(@"Red: %f", components[0]);
+    NSLog(@"Green: %f", components[1]);
+    NSLog(@"Blue: %f", components[2]);
+    NSLog(@"Alpha: %f", CGColorGetAlpha(self.color.CGColor));
+    
+    CGFloat colorValue = components[0] + components[1] + components[2] +components[3];
+    NSLog(@"colorValue = %f", colorValue);
+    
+    if (colorValue < 1.5) {
+        NSLog(@"transcode image should be light");
+    } else {
+        NSLog(@"transcode image should be dark");
+    }
     
     self.transcodeImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"transcoding"]];
     self.transcodeImage.tintColor = [UIColor whiteColor];
-    self.transcodeImage.alpha = 0.54;
+    self.transcodeImage.alpha = 0.87;
     self.transcodeImage.frame = CGRectMake(self.frame.size.width/2 - self.transcodeImage.frame.size.width/2, self.frame.size.height/2 - self.transcodeImage.frame.size.height/2, self.transcodeImage.frame.size.width, self.transcodeImage.frame.size.height);
     [self addSubview:self.transcodeImage];
     
     self.transcodeLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.frame.size.height/2 + 40, self.frame.size.width, 20)];
     self.transcodeLabel.text = @"Processing image";
     self.transcodeLabel.font = [UIFont systemFontOfSize:15 weight:UIFontWeightRegular];
-    self.transcodeLabel.alpha = 0.54;
+    self.transcodeLabel.alpha = 0.87;
     self.transcodeLabel.textAlignment = NSTextAlignmentCenter;
     [self addSubview:self.transcodeLabel];
     
 }
+
+
+
+
+- (UIColor *) GetCurrentPixelColorAtPoint:(CGPoint)point {
+    
+    unsigned char centerPixel[4] = {0};
+
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(centerPixel, 1, 1, 8, 4, colorSpace, kCGBitmapAlphaInfoMask & kCGImageAlphaPremultipliedLast);
+    CGContextTranslateCTM(context, -point.x, -point.y);
+    [self.imageView.layer renderInContext:context];
+    NSLog(@"centerPixel: %d %d %d %d", centerPixel[0], centerPixel[1], centerPixel[2], centerPixel[3]);
+    
+//    self.color should = average RGB value
+    self.color = [UIColor colorWithRed:centerPixel[0]/255.0 green:centerPixel[1]/255.0 blue:centerPixel[2]/255.0 alpha:centerPixel[3]/255.0];
+
+    return self.color;
+}
+
+
+
+
+//- (BOOL)isWallPixel:(UIImage *)image xCoordinate:(int)x yCoordinate:(int)y {
+//    
+//    CFDataRef pixelData = CGDataProviderCopyData(CGImageGetDataProvider(image.CGImage));
+//    const UInt8* data = CFDataGetBytePtr(pixelData);
+//    
+//    int pixelInfo = ((image.size.width  * y) + x ) * 4; // The image is png
+//    
+//    UInt8 red = data[pixelInfo];         // If you need this info, enable it
+//    UInt8 green = data[(pixelInfo + 1)]; // If you need this info, enable it
+//    UInt8 blue = data[pixelInfo + 2];    // If you need this info, enable it
+//    UInt8 alpha = data[pixelInfo + 3];     // I need only this info for my maze game
+//    CFRelease(pixelData);
+//    
+//    UIColor* color = [UIColor colorWithRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:alpha/255.0f]; // The pixel color info
+//    
+//    NSLog(@"color = %@", color);
+//    
+//    if (alpha) return YES;
+//    else return NO;
+//    
+//}
 
 - (void)createTranscodingPlaceHolder{
    
