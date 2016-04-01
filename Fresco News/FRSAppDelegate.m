6078b772e3030d682889416379fef35900784d70
Philip Bernstein <ozetadev@me.com>
2016-04-01 14:51:01 -0400

    sp

--- Fresco News/FRSAppDelegate.m
+++ Fresco News/FRSAppDelegate.m
@@ -36,7 +36,7 @@
     [self configureCoreDataStack];
     [self createItemsWithIcons];
     
-    self.window.rootViewController = [[FRSTabBarController alloc] init];
+    self.window.rootViewController = [[FRSOnboardingViewController alloc] init];
     
     if (launchOptions[UIApplicationLaunchOptionsLocationKey]) {
         [self handleLocationUpdate];
