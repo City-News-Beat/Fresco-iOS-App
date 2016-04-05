e785b81e1a88a46954fc4a7dd549fdaf271b8f1a
Philip Bernstein <ozetadev@me.com>
2016-04-05 13:56:17 -0400

    constraints

--- FRSFileViewController.m
+++ FRSFileViewController.m
@@ -31,6 +31,8 @@ static NSString *imageTile = @"ImageTile";
     [self setupCollectionView];
     [self setupSecondaryUI];
     
+    self.navigationItem.title = @"CHOOSE MEDIA";
+    
 }
 
 -(void)setupSecondaryUI {
@@ -80,10 +82,10 @@ static NSString *imageTile = @"ImageTile";
     fileCollectionView.translatesAutoresizingMaskIntoConstraints = NO;
     
     // constraints on collection view (did collection view manually b/c changing layout after instantiation (i.e from a nib, the spacing can get really messed up.)
-    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:fileCollectionView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1 constant:64];
+    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:fileCollectionView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1 constant:0];
     NSLayoutConstraint *left = [NSLayoutConstraint constraintWithItem:fileCollectionView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1 constant:0];
     NSLayoutConstraint *right = [NSLayoutConstraint constraintWithItem:fileCollectionView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeRight multiplier:1 constant:0];
-    NSLayoutConstraint *bottom = [NSLayoutConstraint constraintWithItem:fileCollectionView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1 constant:-44];
+    NSLayoutConstraint *bottom = [NSLayoutConstraint constraintWithItem:fileCollectionView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1 constant:0];
     
     [self.view addConstraints:@[top, left, bottom, right]];
     [self.view layoutIfNeeded];
