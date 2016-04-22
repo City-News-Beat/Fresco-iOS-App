1b0e5ba27f10f81d472e465da006c031753e5be7
Philip Bernstein <ozetadev@me.com>
2016-04-22 13:50:29 -0400

    fix

--- Fresco News/View Controllers/FRSGalleryExpandedViewController.m
+++ Fresco News/View Controllers/FRSGalleryExpandedViewController.m
@@ -122,7 +122,7 @@
 
 
 -(void)configureScrollView{
-    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, -64, self.view.frame.size.width, self.view.frame.size.height - 64 - 44)];
+    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, -64, self.view.frame.size.width, self.view.frame.size.height - 44)];
     self.scrollView.backgroundColor = [UIColor frescoBackgroundColorDark];
     self.scrollView.delegate = self;
     [self.view addSubview:self.scrollView];
