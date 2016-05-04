4db27befb138bea85f486f7a36608bc128f1cfd2
Omar <omar.elfanek@gmail.com>
2016-05-04 14:12:52 -0400

    assignment picker (wip)

--- Fresco News/View Controllers/FRSUploadViewController.h
+++ Fresco News/View Controllers/FRSUploadViewController.h
@@ -9,6 +9,6 @@
 #import <UIKit/UIKit.h>
 #import "FRSBaseViewController.h"
 
-@interface FRSUploadViewController : FRSBaseViewController
+@interface FRSUploadViewController : FRSBaseViewController <UIScrollViewDelegate, UITableViewDelegate, UITableViewDataSource>
 
 @end
--- Fresco News/View Controllers/FRSUploadViewController.m
+++ Fresco News/View Controllers/FRSUploadViewController.m
@@ -7,12 +7,19 @@
 //
 
 #import "FRSUploadViewController.h"
+//#import "FRSAssignmentPickerTableViewCell.xib"
 
 @interface FRSUploadViewController ()
+
+@property (strong, nonatomic) UIView *navigationBarView;
+@property (strong, nonatomic) UIScrollView *scrollView;
+@property (strong, nonatomic) UITableView *tableView;
 
 @end
 
 @implementation FRSUploadViewController
+
+static NSString * const cellIdentifier = @"assignment-cell";
 
 -(void)viewDidLoad {
     [super viewDidLoad];
@@ -28,15 +35,18 @@
     self.navigationController.navigationBarHidden = YES;
     
     [self configureNavigationBar];
+    [self configureScrollView];
+    [self configureTableView];
+    [self configureBottomBar];
 }
 
 -(void)configureNavigationBar {
 
     /* Configure sudo navigationBar */
         // Used UIView instead of UINavigationBar for increased flexibility when animating
-    UIView *navigationBar = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 64)];
-    navigationBar.backgroundColor = [UIColor frescoOrangeColor];
-    [self.view addSubview:navigationBar];
+    self.navigationBarView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 64)];
+    self.navigationBarView.backgroundColor = [UIColor frescoOrangeColor];
+    [self.view addSubview:self.navigationBarView];
     
     /* Configure backButton */
     UIButton *backButton = [UIButton buttonWithType:UIButtonTypeSystem];
@@ -44,22 +54,75 @@
     [backButton setImage:[UIImage imageNamed:@"back-arrow-light"] forState:UIControlStateNormal];
     [backButton setTintColor:[UIColor whiteColor]];
     [backButton addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
-    [navigationBar addSubview:backButton];
+    [self.navigationBarView addSubview:backButton];
     
     /* Configure squareButton */
     UIButton *squareButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    squareButton.frame = CGRectMake(navigationBar.frame.size.width-12-24, 30, 24, 24);
+    squareButton.frame = CGRectMake(self.navigationBarView.frame.size.width-12-24, 30, 24, 24);
     [squareButton setImage:[UIImage imageNamed:@"square"] forState:UIControlStateNormal];
     [squareButton setTintColor:[UIColor whiteColor]];
     [squareButton addTarget:self action:@selector(square) forControlEvents:UIControlEventTouchUpInside];
-    [navigationBar addSubview:squareButton];
+    [self.navigationBarView addSubview:squareButton];
     
     /* Configure titleLabel */
     UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.view.frame.size.width/2 -66/2, 35, 66, 19)];
     [titleLabel setFont:[UIFont notaBoldWithSize:17]];
     [titleLabel setText:@"GALLERY"];
     [titleLabel setTextColor:[UIColor whiteColor]];
-    [navigationBar addSubview:titleLabel];
+    [self.navigationBarView addSubview:titleLabel];
+}
+
+-(void)configureBottomBar {
+    
+    /* Configure bottom container */
+    UIView *bottomContainer = [[UIView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height -44, self.view.frame.size.width, 44)];
+    bottomContainer.backgroundColor = [UIColor frescoBackgroundColorLight];
+    [self.view addSubview:bottomContainer];
+    
+    UIView *bottomContainerLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 1)];
+    bottomContainerLine.backgroundColor = [UIColor frescoShadowColor];
+    [bottomContainer addSubview:bottomContainerLine];
+    
+    /* Configure bottom bar */
+    //Configure Twitter post button
+    UIButton *twitterButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [twitterButton addTarget:self action:@selector(postToTwitter) forControlEvents:UIControlEventTouchDown];
+    UIImage *twitter = [[UIImage imageNamed:@"twitter-icon"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    [twitterButton setImage:twitter forState:UIControlStateNormal];
+    twitterButton.frame = CGRectMake(16, 10, 24, 24);
+    [bottomContainer addSubview:twitterButton];
+    
+    //Configure Facebook post button
+    UIButton *facebookButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [twitterButton addTarget:self action:@selector(postToFacebook) forControlEvents:UIControlEventTouchDown];
+    UIImage *facebook = [[UIImage imageNamed:@"facebook-icon"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    [facebookButton setImage:facebook forState:UIControlStateNormal];
+    facebookButton.frame = CGRectMake(56, 10, 24, 24);
+    [bottomContainer addSubview:facebookButton];
+    
+    //Configure anonymous posting button
+    UIButton *anonymousButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [anonymousButton addTarget:self action:@selector(postAnonymously) forControlEvents:UIControlEventTouchDown];
+    UIImage *eye = [[UIImage imageNamed:@"eye-26"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    [anonymousButton setImage:eye forState:UIControlStateNormal];
+    anonymousButton.frame = CGRectMake(96, 10, 24, 24);
+    [bottomContainer addSubview:anonymousButton];
+    
+    //Configure next button
+    UIButton *sendButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [sendButton.titleLabel setFont:[UIFont notaBoldWithSize:17]];
+    [sendButton setTintColor:[UIColor frescoLightTextColor]];
+    sendButton.frame = CGRectMake(self.view.frame.size.width-64, 0, 64, 44);
+    [sendButton setTitle:@"SEND" forState:UIControlStateNormal];
+    [sendButton addTarget:self action:@selector(send) forControlEvents:UIControlEventTouchUpInside];
+    sendButton.userInteractionEnabled = NO;
+    [bottomContainer addSubview:sendButton];
+}
+
+
+#pragma mark - UIScrollView
+
+-(void)configureScrollView {
     
     /* Height for scrollView */
     int height;
@@ -72,58 +135,54 @@
     }
     
     /* Configure scrollView */
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, height)];
-    scrollView.backgroundColor = [UIColor redColor];
-    scrollView.alpha = 0.1;
-    [navigationBar addSubview:scrollView];
+    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, height)];
+    self.scrollView.delegate = self;
+    [self.navigationBarView addSubview:self.scrollView];
     
-    /* Configure bottom container */
-    UIView *bottomContainer = [[UIView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height -44, self.view.frame.size.width, 44)];
-    bottomContainer.backgroundColor = [UIColor frescoBackgroundColorLight];
-    [self.view addSubview:bottomContainer];
 
-    UIView *bottomContainerLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 1)];
-    bottomContainerLine.backgroundColor = [UIColor frescoShadowColor];
-    [bottomContainer addSubview:bottomContainerLine];
     
     
-    /* Configure bottom bar */
-        //Configure Twitter post button
-    UIButton *twitterButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    [twitterButton addTarget:self action:@selector(postToTwitter) forControlEvents:UIControlEventTouchDown];
-    UIImage *twitter = [[UIImage imageNamed:@"twitter-icon"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    [twitterButton setImage:twitter forState:UIControlStateNormal];
-    twitterButton.frame = CGRectMake(16, 10, 24, 24);
-    [bottomContainer addSubview:twitterButton];
     
-        //Configure Facebook post button
-    UIButton *facebookButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    [twitterButton addTarget:self action:@selector(postToFacebook) forControlEvents:UIControlEventTouchDown];
-    UIImage *facebook = [[UIImage imageNamed:@"facebook-icon"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    [facebookButton setImage:facebook forState:UIControlStateNormal];
-    facebookButton.frame = CGRectMake(56, 10, 24, 24);
-    [bottomContainer addSubview:facebookButton];
-    
-        //Configure anonymous posting button
-    UIButton *anonymousButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    [anonymousButton addTarget:self action:@selector(postAnonymously) forControlEvents:UIControlEventTouchDown];
-    UIImage *eye = [[UIImage imageNamed:@"eye-26"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    [anonymousButton setImage:eye forState:UIControlStateNormal];
-    anonymousButton.frame = CGRectMake(96, 10, 24, 24);
-    [bottomContainer addSubview:anonymousButton];
-    
-        //Configure next button
-    UIButton *sendButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    [sendButton.titleLabel setFont:[UIFont notaBoldWithSize:17]];
-    [sendButton setTintColor:[UIColor frescoLightTextColor]];
-    sendButton.frame = CGRectMake(self.view.frame.size.width-64, 0, 64, 44);
-    [sendButton setTitle:@"SEND" forState:UIControlStateNormal];
-    [sendButton addTarget:self action:@selector(next:) forControlEvents:UIControlEventTouchUpInside];
-    sendButton.userInteractionEnabled = NO;
-    [bottomContainer addSubview:sendButton];
-    
+    /* DEBUG */
+    self.scrollView.backgroundColor = [UIColor redColor];
+    self.scrollView.alpha = 0.1;
 }
 
+
+#pragma mark - UITableView
+
+
+-(void)configureTableView {
+    
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, self.scrollView.frame.size.height +64, self.view.frame.size.width, 200) style:UITableViewStylePlain];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    [self.view addSubview:self.tableView];
+    [self.tableView registerNib:[UINib nibWithNibName:@"FRSAssignmentPickerTableViewCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:cellIdentifier];
+    
+    /* DEBUG */
+    self.tableView.backgroundColor = [UIColor greenColor];
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 44;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    return cell;
+}
 
 #pragma mark - Actions
 
@@ -145,6 +204,7 @@
     
 }
 
+
 /* Bottom Bar */
     //Post to Facebook
 -(void)postToFacebook {
@@ -161,25 +221,11 @@
     
 }
 
-
-
-
-
-
     /* TODO */
     //  Pull selected photos/videos from FRSFileViewController.m
     //  Post to social toggles, should save options from previous view controller
-            //Ideally would segue similarly to Onboard -> Login, while keeping tab at the bototm and sliding nav bar up
+            //Ideally would segue similarly to Onboard -> Login, while keeping tab at the bottom and sliding nav bar up
     // Anonymous posting (?)
     // Nav bar show/hide on scroll
-
-
-
-
-
-
-
-
-
 
 @end
