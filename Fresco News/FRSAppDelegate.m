81b57716b78dcd60626c69a1ba9260b34c639fb9
Omar <omar.elfanek@gmail.com>
2016-05-09 14:32:37 -0400

    check for gallery cell

--- Fresco News/FRSAppDelegate.m
+++ Fresco News/FRSAppDelegate.m
@@ -40,7 +40,7 @@
     [self configureCoreDataStack];
     
  
-    if ([[FRSAPIClient sharedClient] isAuthenticated]) {
+    if ([[FRSAPIClient sharedClient] isAuthenticated] || TRUE) {
         self.tabBarController = [[FRSTabBarController alloc] init];
         self.window.rootViewController = self.tabBarController;
         [self createItemsWithIcons];
