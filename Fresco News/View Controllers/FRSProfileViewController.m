54d5ce5f31ad9577d7e3d36b0f6ef103b1e02f26
Philip Bernstein <ozetadev@me.com>
2016-06-14 15:42:50 -0700

    fix2

--- Fresco News/View Controllers/FRSProfileViewController.m
+++ Fresco News/View Controllers/FRSProfileViewController.m
@@ -493,7 +493,7 @@
                 cell = [[FRSGalleryCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"gallery-cell"];
             }
         }
-        else if ([[[self.galleries objectAtIndex:indexPath.row] class] isSubclassOfClass:[FRSStoryCell class]]) {
+        else if ([[[self.galleries objectAtIndex:indexPath.row] class] isSubclassOfClass:[FRSStory class]]) {
             cell = [tableView dequeueReusableCellWithIdentifier:@"story-cell"];
             
             if (!cell){
