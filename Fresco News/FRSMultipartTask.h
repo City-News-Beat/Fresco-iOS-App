66da045d8d1f480cd8c42b5b6d25bb09c58b3b7e
Philip Bernstein <ozetadev@me.com>
2016-07-11 20:06:20 -0400

    playing w optimal network

--- Fresco News/FRSMultipartTask.h
+++ Fresco News/FRSMultipartTask.h
@@ -11,7 +11,7 @@
 #import "FRSUploadTask.h"
 
 static int const chunkSize = 5;
-static int const maxConcurrentUploads = 3;
+static int const maxConcurrentUploads = 5;
 static int const megabyteDefinition = 1048576; // bytes -> mb
 
 static int const chunkMaxFailures = 2; // resets between chunks (otherwise behaves like superclass)
