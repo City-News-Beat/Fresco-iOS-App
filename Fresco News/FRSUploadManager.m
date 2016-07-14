52f6b502985615e1f06943c979f2dbd5ca3a51a0
Philip Bernstein <ozetadev@me.com>
2016-07-14 13:56:35 -0400

    images in upload manager

--- Fresco News/FRSUploadManager.m
+++ Fresco News/FRSUploadManager.m
@@ -41,7 +41,7 @@
     [task start];
 }
 
--(void)addMultipartTaskForAsset:(PHAsset *)asset url:(NSArray *)urls post:(NSDictionary *)post {
+-(void)addMultipartTaskForAsset:(PHAsset *)asset urls:(NSArray *)urls post:(NSDictionary *)post {
     
     PHVideoRequestOptions* options = [[PHVideoRequestOptions alloc] init];
     options.version = PHVideoRequestOptionsVersionOriginal;
@@ -74,10 +74,8 @@
                     postCompletionDigest[@"uploadId"] = post[@"uploadId"];
                     postCompletionDigest[@"key"] = post[@"key"];
                     
-                    NSLog(@"%@", postCompletionDigest);
-                    
                     [[FRSAPIClient sharedClient] completePost:post[@"post_id"] params:postCompletionDigest completion:^(id responseObject, NSError *error) {
-                        NSLog(@"%@ %@", responseObject, error);
+                        NSLog(@"POST COMPLETED: %@", (error == Nil) ? @"TRUE" : @"FALSE");
                     }];
                 }];
             }
@@ -85,7 +83,35 @@
         
         [weakSelf addTask:multipartTask];
     }];
-    
+}
+
+-(void)addTaskForImageAsset:(PHAsset *)asset url:(NSURL *)url post:(NSDictionary *)post {
+    [[PHImageManager defaultManager] requestImageDataForAsset:asset options:nil resultHandler:^(NSData *imageData, NSString *dataUTI, UIImageOrientation orientation, NSDictionary *info) {
+        
+        FRSUploadTask *task = [[FRSUploadTask alloc] init];
+        
+        [task createUploadFromData:imageData destination:url progress:^(id task, int64_t bytesSent, int64_t totalBytesSent, int64_t totalBytesExpectedToSend) {
+            NSLog(@"UPLOADING");
+        } completion:^(id task, NSData *responseData, NSError *error, BOOL success, NSURLResponse *response) {
+            if (success) {
+                if (success) {
+                    NSDictionary *headers = [(NSHTTPURLResponse *)response allHeaderFields];
+                    NSString *eTag = headers[@"Etag"];
+                    
+                    NSMutableDictionary *postCompletionDigest = [[NSMutableDictionary alloc] init];
+                    postCompletionDigest[@"eTags"] = @[eTag];
+                    postCompletionDigest[@"uploadId"] = post[@"uploadId"];
+                    postCompletionDigest[@"key"] = post[@"key"];
+                    [[FRSAPIClient sharedClient] completePost:post[@"post_id"] params:postCompletionDigest completion:^(id responseObject, NSError *error) {
+                        NSLog(@"%@ %@", responseObject, error);
+                        
+                    }];
+                }
+            }
+        }];
+        
+        [weakSelf addTask:task];
+    }];
 }
 
 -(void)pause {
