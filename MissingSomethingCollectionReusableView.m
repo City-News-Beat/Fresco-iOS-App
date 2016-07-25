3acbeacac61fda4dd53e67c26fb76c08ffcd123e
wafflez180 <arthurdearaujo@Arthurs-MacBook-Pro.local>
2016-07-25 14:39:26 -0400

    Changed Missing Something constraints and spacings

--- MissingSomethingCollectionReusableView.m
+++ MissingSomethingCollectionReusableView.m
@@ -18,6 +18,9 @@
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *chatWithLeftConstraint;
 @property (weak, nonatomic) IBOutlet UIButton *settingsButton;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *settingsRightConstraint;
+@property (weak, nonatomic) IBOutlet UITextView *missingSomethingMainText;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *missingSomeMainTextTopCon;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *questionTopConstraint;
 @property BOOL isSetup;
 @end
 
@@ -36,12 +39,67 @@
         self.textView.delegate = (id<UITextViewDelegate>)self;
     }    
     self.textView.allowsEditingTextAttributes = false;
+    NSString *attribText = [NSString stringWithFormat:@"We can only verify photos and videos from the past 24 hours, taken with location data. If nothing is showing up, make sure Location is enabled in                 for next time."];
+    NSMutableParagraphStyle *paragraphStyle = NSMutableParagraphStyle.new;
+    paragraphStyle.minimumLineHeight = 20;
+    paragraphStyle.maximumLineHeight = 20;
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+    NSDictionary *attribs = @{
+                              NSForegroundColorAttributeName:[[UIColor blackColor] colorWithAlphaComponent:0.54],
+                              NSFontAttributeName: [UIFont systemFontOfSize:15 weight:UIFontWeightRegular],
+                              NSParagraphStyleAttributeName: paragraphStyle
+                              };
+    
+    NSMutableAttributedString *attributedText =[[NSMutableAttributedString alloc] initWithString:attribText attributes:attribs];
+    
+    //[attributedText addAttribute:NSKernAttributeName
+    //                value:@(1.03)
+    //                range:NSMakeRange(0, attribText.length-1)];
+
+    self.missingSomethingMainText.attributedText = attributedText;
     if(IS_IPHONE_6_PLUS){
-        self.settingsRightConstraint.constant = -48;
+        self.settingsRightConstraint.constant = -9;
+        self.settingsTopConstraint.constant = 54;
+        
+        NSString *attribText = [NSString stringWithFormat:@"We can only verify photos and videos from the\n past 24 hours, taken with location data. If\n nothing is showing up, make sure Location is\n enabled in                 for next time."];
+        NSMutableParagraphStyle *paragraphStyle = NSMutableParagraphStyle.new;
+        paragraphStyle.lineSpacing = 1.2;
+        paragraphStyle.alignment = NSTextAlignmentCenter;
+        NSDictionary *attribs = @{
+                                  NSForegroundColorAttributeName:[[UIColor blackColor] colorWithAlphaComponent:0.54],
+                                  NSFontAttributeName: [UIFont systemFontOfSize:15 weight:UIFontWeightRegular],
+                                  NSParagraphStyleAttributeName: paragraphStyle
+                                  };
+
+        NSMutableAttributedString *attributedText =[[NSMutableAttributedString alloc] initWithString:attribText attributes:attribs];
+    
+        [self.missingSomethingMainText setAttributedText:attributedText];
+        self.questionTopConstraint.constant = -5;
+        self.chatWithTopConstraint.constant = -4;
+        self.missingSomeMainTextTopCon.constant = 75;
     }else if(IS_IPHONE_5){
-        self.settingsTopConstraint.constant = 67;
-        self.settingsRightConstraint.constant = -10;
-        self.chatWithTopConstraint.constant = 10;
+        NSString *attribText = [NSString stringWithFormat:@"We can only verify photos and videos from the past 24 hours, taken with location data. If nothing is showing up, make sure Location is enabled in\n                  for next time."];
+        NSMutableParagraphStyle *paragraphStyle = NSMutableParagraphStyle.new;
+        paragraphStyle.minimumLineHeight = 20;
+        paragraphStyle.maximumLineHeight = 20;
+        paragraphStyle.alignment = NSTextAlignmentCenter;
+        NSDictionary *attribs = @{
+                                  NSForegroundColorAttributeName:[[UIColor blackColor] colorWithAlphaComponent:0.54],
+                                  NSFontAttributeName: [UIFont systemFontOfSize:15 weight:UIFontWeightRegular],
+                                  NSParagraphStyleAttributeName: paragraphStyle
+                                  };
+        
+        NSMutableAttributedString *attributedText =[[NSMutableAttributedString alloc] initWithString:attribText attributes:attribs];
+        
+        //[attributedText addAttribute:NSKernAttributeName
+        //                value:@(1.03)
+        //                range:NSMakeRange(0, attribText.length-1)];
+        
+        self.missingSomethingMainText.attributedText = attributedText;
+
+        self.settingsTopConstraint.constant = 73;
+        self.settingsRightConstraint.constant = -43;
+        self.chatWithTopConstraint.constant = 15;
         self.chatWithRightConstraint.constant = 110;
         [super removeConstraint:self.chatWithLeftConstraint];
         self.questionLeftConstraint.constant = 50;
