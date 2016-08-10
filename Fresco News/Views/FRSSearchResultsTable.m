f315e7ebc8b7a63f74a4962b2750a41346b1c8bc
Philip Bernstein <ozetadev@me.com>
2016-08-10 11:08:38 -0400

    new cells etc (lots of updates sorry)

--- Fresco News/Views/FRSSearchResultsTable.m
+++ Fresco News/Views/FRSSearchResultsTable.m
@@ -20,4 +20,38 @@
     self.galleries = results[2];
     [self reloadData];
 }
+
+-(void)commonInit {
+    
+}
+
+-(instancetype)init {
+    self = [super init];
+    
+    if (self) {
+        [self commonInit];
+    }
+    
+    return self;
+}
+
+-(instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        [self commonInit];
+    }
+    
+    return self;
+}
+
+-(instancetype)initWithFrame:(CGRect)frame style:(UITableViewStyle)style {
+    self = [super initWithFrame:frame style:style];
+    
+    if (self) {
+        [self commonInit];
+    }
+    
+    return self;
+}
 @end
