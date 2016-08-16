3b8327afcf43e6f7ccdb976c285814586c2a9cf9
Philip Bernstein <ozetadev@me.com>
2016-08-16 17:08:32 -0400

    better log for location error

---
+++ FRSPaymentViewController.h
@@ -0,0 +1,16 @@
+//
+//  FRSPaymentViewController.h
+//  Fresco
+//
+//  Created by Philip Bernstein on 8/16/16.
+//  Copyright © 2016 Fresco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FRSBaseViewController.h"
+#import "Fresco.h"
+#import "FRSAPIClient.h"
+
+@interface FRSPaymentViewController : FRSBaseViewController
+
+@end
