9f7ddca23b85526e4378aebeb359a716970212cb
Philip Bernstein <ozetadev@me.com>
2016-08-24 16:12:30 -0400

    fix blue

--- Fresco News/View Controllers/FRSProfileViewController.m
+++ Fresco News/View Controllers/FRSProfileViewController.m
@@ -141,7 +141,7 @@
 
 -(void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
     if (isLoadingUser) {
         return;
     }
