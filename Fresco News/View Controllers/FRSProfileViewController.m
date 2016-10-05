4b3deb1b8b41b1cbc156fb72ddee27dc690dbab0
Omar <omar.elfanek@gmail.com>
2016-10-05 14:32:39 -0400

    installed MGSwipeTableCell

--- Fresco News/View Controllers/FRSProfileViewController.m
+++ Fresco News/View Controllers/FRSProfileViewController.m
@@ -215,7 +215,7 @@
         
         //IF SUCCESS, PRESENT ALERT
         FRSAlertView *alert = [[FRSAlertView alloc] initWithTitle:@"BLOCKED" message: [NSString stringWithFormat:@"You won’t see posts from @%@ anymore.", _representedUser.username] actionTitle:@"UNDO" cancelTitle:@"OK" cancelTitleColor:[UIColor frescoBlueColor] delegate:self];
-        [alert show];
+        [alert show];        
         
         [view dismissViewControllerAnimated:YES completion:nil];
     }];
