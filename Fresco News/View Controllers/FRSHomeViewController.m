7e5b961984d24b5be3af05be7a48e1c3fb1f81e6
Philip Bernstein <ozetadev@me.com>
2016-10-19 13:32:45 -0400

    perform block on reload

--- Fresco News/View Controllers/FRSHomeViewController.m
+++ Fresco News/View Controllers/FRSHomeViewController.m
@@ -244,34 +244,32 @@
     [self.followingTable reloadFollowing];
     
     [[FRSAPIClient sharedClient] fetchGalleriesWithLimit:self.dataSource.count offsetGalleryID:Nil completion:^(NSArray *galleries, NSError *error) {
-        
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            
-            NSInteger index = 0;
-            
-            for (NSDictionary *gallery in galleries) {
-                NSString *galleryID = gallery[@"id"];
-                NSInteger galleryIndex = [self galleryExists:galleryID];
+            [self.appDelegate.managedObjectContext performBlock:^{
+                NSInteger index = 0;
                 
-                if (galleryIndex < 0 || galleryIndex >= self.dataSource.count) {
-                    continue;
+                for (NSDictionary *gallery in galleries) {
+                    NSString *galleryID = gallery[@"id"];
+                    NSInteger galleryIndex = [self galleryExists:galleryID];
+                    
+                    if (galleryIndex < 0 || galleryIndex >= self.dataSource.count) {
+                        continue;
+                    }
+                    
+                    FRSGallery *galleryToSave = [self.dataSource objectAtIndex:galleryIndex];
+                    [galleryToSave configureWithDictionary:gallery context:[self.appDelegate managedObjectContext]];
+                    [galleryToSave setValue:[NSNumber numberWithInteger:index] forKey:@"index"];
+                    
+                    index++;
                 }
                 
-                FRSGallery *galleryToSave = [self.dataSource objectAtIndex:galleryIndex];
-                [galleryToSave configureWithDictionary:gallery context:[self.appDelegate managedObjectContext]];
-                [galleryToSave setValue:[NSNumber numberWithInteger:index] forKey:@"index"];
-                
-                index++;
-            }
-            
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [self.tableView reloadData];
-                isLoading = FALSE;
-                [self.tableView dg_stopLoading];
-                [self.followingTable dg_stopLoading];
-            });
-        });
-    }];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self.tableView reloadData];
+                    isLoading = FALSE;
+                    [self.tableView dg_stopLoading];
+                    [self.followingTable dg_stopLoading];
+                });
+            }];
+        }];
 }
 
 -(void)tableView:(UITableView *)tableView didEndDisplayingCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
