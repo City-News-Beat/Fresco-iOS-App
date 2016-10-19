8a8ec3be434d7b248260e254a54b5e9597831a84
Philip Bernstein <ozetadev@me.com>
2016-10-19 12:55:31 -0400

    payment selection fix

--- FRSPaymentViewController.m
+++ FRSPaymentViewController.m
@@ -103,7 +103,10 @@ static NSString *addPaymentCell = @"addPaymentCell";
         
         NSDictionary *payment = [self.payments objectAtIndex:indexPath.row];
         NSString *paymentID = payment[@"id"];
-        
+        NSString *digits = [NSString stringWithFormat:@"%@ (%@)", payment[@"brand"], payment[@"last4"]];
+        [[[FRSAPIClient sharedClient] authenticatedUser] setValue:digits forKey:@"creditCardDigits"];
+        [(FRSAppDelegate *)[[UIApplication sharedApplication] delegate] saveContext];
+
         [[FRSAPIClient sharedClient] makePaymentActive:paymentID completion:^(id responseObject, NSError *error) {
             if (!error) {
                 [self resetOtherPayments:paymentID];
