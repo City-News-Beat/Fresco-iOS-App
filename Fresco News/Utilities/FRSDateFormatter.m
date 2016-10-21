2924c76c44be4f7a38eb340e23039e001a1007c7
Philip Bernstein <ozetadev@me.com>
2016-10-21 16:08:16 -0400

    date formatting fix

--- Fresco News/Utilities/FRSDateFormatter.m
+++ Fresco News/Utilities/FRSDateFormatter.m
@@ -18,7 +18,34 @@
     return [NSDate dateWithTimeIntervalSince1970:seconds];
 }
 
-+(NSString *)dateStringGalleryFormatFromDate:(NSDate *)date{
++ (NSString *)daySuffixForDate:(NSDate *)date {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSInteger dayOfMonth = [calendar component:NSCalendarUnitDay fromDate:date];
+    switch (dayOfMonth) {
+        case 1:
+        case 21:
+        case 31: return @"st";
+        case 2:
+        case 22: return @"nd";
+        case 3:
+        case 23: return @"rd";
+        default: return @"th";
+    }
+}
+
++(NSString *)dateStringGalleryFormatFromDate:(NSDate *)date {
+    NSTimeInterval sinceStart = [date timeIntervalSinceNow];
+    sinceStart *= -1;
+    
+    if (sinceStart >= (24 * 60 * 60)) {
+        NSTimeInterval secondsFromGMT = [[NSTimeZone localTimeZone] secondsFromGMT];
+        NSDate *correctDate = [date dateByAddingTimeInterval:secondsFromGMT];
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        [formatter setTimeZone:[NSTimeZone localTimeZone]];
+        [formatter setDateFormat:@"hh:mm a, MMMM dd"];
+        return [[formatter stringFromDate:correctDate] stringByAppendingString:[FRSDateFormatter daySuffixForDate:date]];
+    }
+    
     NSTimeInterval secondsFromGMT = [[NSTimeZone localTimeZone] secondsFromGMT];
     NSDate *correctDate = [date dateByAddingTimeInterval:secondsFromGMT];
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
