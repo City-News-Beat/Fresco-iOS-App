8f87e18d0041289e316fe52799af453f37ac7a77
Omar Elfanek <omar.elfanek@gmail.com>
2016-10-25 17:19:51 -0400

    typo

--- Fresco News/Views/FRSStoryView.m
+++ Fresco News/Views/FRSStoryView.m
@@ -80,6 +80,7 @@
     /*UIView *bottomLine = [[UIView alloc] initWithFrame:CGRectMake(0, self.frame.size.height, self.frame.size.width, 0.5)];
     bottomLine.backgroundColor = [UIColor frescoShadowColor];
     [self addSubview:bottomLine];*/
+}
 
 -(void)configureTopContainer{
     
