b74c927f03d80202b617910fdbd2f23f918f2800
Omar Elfanek <omar.elfanek@gmail.com>
2016-11-22 11:56:58 -0500

    add username/name to comments

--- Fresco News/View Controllers/FRSCommentCell.m
+++ Fresco News/View Controllers/FRSCommentCell.m
@@ -47,21 +47,44 @@
     [self.commentTextView sizeToFit];
     self.commentTextView.delegate = delegate;
     
+    BOOL userHasName = NO;
+    BOOL userHasUsername = NO;
+    
     NSDate *date = [comment updatedAt];
     self.timestampLabel.text = [FRSDateFormatter relativeTimeFromDate:date];
     
     
     
     if (![[comment userDictionary][@"full_name"] isEqual:[NSNull null]] && ![[comment userDictionary][@"full_name"] isEqualToString:@""]) {
+        userHasName = YES;
+    }
+    
+    if (![[comment userDictionary][@"username"] isEqual:[NSNull null]] && ![[comment userDictionary][@"username"] isEqualToString:@""]) {
+        userHasUsername = YES;
+    }
+    
+    
+    if (userHasName && userHasUsername) {
         self.nameLabel.text = [comment userDictionary][@"full_name"];
-    } else if (![[comment userDictionary][@"username"] isEqual:[NSNull null]] && ![[comment userDictionary][@"username"] isEqualToString:@""]) {
+        self.timestampLabel.text = [NSString stringWithFormat:@"@%@ • %@", [comment userDictionary][@"username"], [FRSDateFormatter relativeTimeFromDate:date]];
+    } else if (!userHasName && userHasUsername) {
         self.nameLabel.text = [NSString stringWithFormat:@"@%@", [comment userDictionary][@"username"]];
-    } else {
-        self.nameLabel.text = @"@username";
+        self.timestampLabel.text = [FRSDateFormatter relativeTimeFromDate:date];
+    } else if (userHasName && !userHasUsername) {
+        self.nameLabel.text = [NSString stringWithFormat:@"@%@", [comment userDictionary][@"first_name"]];
+        self.timestampLabel.text = [FRSDateFormatter relativeTimeFromDate:date];
+    } else if (!userHasUsername && !userHasUsername) {
         self.timestampLabel.transform = CGAffineTransformMakeTranslation(-8, 0);
     }
     
 
+    
+    
+    
+    
+    
+    
+    
 //    Calling size to fit here scales the textview down so the user can tap on the comment cell
 //    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
 //        [self.commentTextView sizeToFit];
