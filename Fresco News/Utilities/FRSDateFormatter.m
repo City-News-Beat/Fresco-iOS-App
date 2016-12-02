d72756eee4012e9c65cc40b34cf9fbfd52c52802
Philip Bernstein <ozetadev@me.com>
2016-12-02 12:37:00 -0500

    date fix

--- Fresco News/Utilities/FRSDateFormatter.m
+++ Fresco News/Utilities/FRSDateFormatter.m
@@ -50,63 +50,70 @@
     NSDate *correctDate = [date dateByAddingTimeInterval:secondsFromGMT];
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
     [formatter setTimeZone:[NSTimeZone localTimeZone]];
-    [formatter setDateFormat:@"h:mm a"];
+    [formatter setDateFormat:@"hh:mm a"];
     return [formatter stringFromDate:correctDate];
 }
 
-
-+(NSString *)relativeTimeFromDate:(NSDate *)compareDate {
++(NSString *)timestampStringFromDate:(NSDate *)date {
+    NSTimeInterval doubleDiff = [date timeIntervalSinceNow];
+    long diff = (long) doubleDiff;
+    int seconds = diff % 60;
+    diff = diff / 60;
+    int minutes = diff % 60;
+    diff = diff / 60;
+    int hours = diff % 24;
+    int days = diff / 24;
     
-    NSTimeInterval timeInterval = [[NSDate date] timeIntervalSinceDate:compareDate];
+    NSString *timestampString;
     
-    int temp = 0;
-    NSString *result;
-    if (timeInterval < 60) {
-        result = [NSString stringWithFormat:@"Just now"];
-    } else if ((temp = timeInterval/60) <60){
-        result = [NSString stringWithFormat:@"%d minutes ago", temp];
-        if ([result isEqualToString:@"1 minutes ago"]) {
-            result = @"1 minute ago";
+    if (days < 0) {
+        days *= -1;
+    }
+    if (hours < 0) {
+        hours *= -1;
+    }
+    if (minutes < 0) {
+        minutes *= -1;
+    }
+    if (seconds < 0) {
+        seconds *= -1;
+    }
+    
+    if (days != 0) {
+        timestampString = [NSString stringWithFormat:@"%d days ago", days];
+        if (days >= 1 && days < 2) {
+            timestampString = [NSString stringWithFormat:@"%d day ago", days];
         }
-    } else if ((temp = temp/60) <24){
-        result = [NSString stringWithFormat:@"%d hours ago", temp];
-        if ([result isEqualToString:@"1 hours ago"]) {
-            result = @"1 hour ago";
+    } else if (hours != 0) {
+        timestampString = [NSString stringWithFormat:@"%d hours ago", hours];
+        if (hours == 1 && hours < 2) {
+            timestampString = [NSString stringWithFormat:@"%d hour ago", hours];
         }
-    } else {
-        temp = temp / 24;
-        result = [NSString stringWithFormat:@"%d days ago", temp];
-        if ([result isEqualToString:@"1 days ago"]) {
-            result = @"1 day ago";
+    } else if (minutes != 0) {
+        timestampString = [NSString stringWithFormat:@"%d minutes ago", minutes];
+        if (minutes == 1 && minutes < 2) {
+            timestampString = [NSString stringWithFormat:@"%d minute ago", minutes];
+        }
+    } else if (seconds != 0) {
+        timestampString = [NSString stringWithFormat:@"%d seconds ago", seconds];
+        if (seconds == 1 && seconds <2) {
+            timestampString = [NSString stringWithFormat:@"%d second ago", seconds];
         }
     }
-    return result;
+    
+    if ([timestampString containsString:@"-"]) {
+        NSCharacterSet *trim = [NSCharacterSet characterSetWithCharactersInString:@"-"];
+        timestampString = [[timestampString componentsSeparatedByCharactersInSet:trim] componentsJoinedByString:@""];
+    }
+    
+    return timestampString;
 }
 
-+(NSString *)dateDifference:(NSDate *)date {
-    const NSTimeInterval secondsPerDay = 60 * 60 * 24;
-    NSTimeInterval diff = [date timeIntervalSinceNow] * -1.0;
-
-    diff /= secondsPerDay;
-    
-    if (diff < 1) {
-        return @"today";
-    } else if (diff < 2) {
-        return @"yesterday";
-    } else {
-        NSTimeInterval secondsFromGMT = [[NSTimeZone localTimeZone] secondsFromGMT];
-        NSDate *correctDate = [date dateByAddingTimeInterval:secondsFromGMT];
-        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-        [formatter setTimeZone:[NSTimeZone localTimeZone]];
-        [formatter setDateFormat:@"MMMM d"];
-        
-        // Display year only if <1yr
-        if (diff >= 365) {
-            [formatter setDateFormat:@"MMMM d, yyy"];
-        }
-        return [formatter stringFromDate:correctDate];
-    }
-
+//temp method
++(NSString *)dateStringFromDate:(NSDate *)date{
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateStyle = NSDateFormatterLongStyle;
+    return [formatter stringFromDate:date];
 }
 
 @end
