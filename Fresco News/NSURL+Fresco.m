b6ba30d8cac41a7fe783dc4aa8fab57a5891db8a
Arthur De Araujo <wafflez180@yahoo.com>
2017-01-20 16:39:18 -0500

    Reformatting

--- Fresco News/NSURL+Fresco.m
+++ Fresco News/NSURL+Fresco.m
@@ -1,6 +1,6 @@
 //
 //  NSURL+Fresco.m
-//  
+//
 //
 //  Created by Arthur De Araujo on 1/17/17.
 //
@@ -10,21 +10,20 @@
 
 @implementation NSURL (Fresco)
 
-
-- (id) initWithString:(NSString *)URLString width:(float)width{
+- (id)initWithString:(NSString *)URLString width:(float)width {
     return [self initWithString:[self URLResizedFromURLString:URLString width:width]];
 }
 
-- (NSString *)URLResizedFromURLString:(NSString *)url width:(float)width{
+- (NSString *)URLResizedFromURLString:(NSString *)url width:(float)width {
     NSString *adjustedURL = url;
-    
+
     if ([adjustedURL containsString:@"cdn.fresconews"]) {
         NSString *adjustedSize = [NSString stringWithFormat:@"%d", (int)width];
         adjustedSize = [@"/images/" stringByAppendingString:adjustedSize];
         adjustedURL = [adjustedURL stringByReplacingOccurrencesOfString:@"/images" withString:adjustedSize];
     }
-    
-    return  adjustedURL;
+
+    return adjustedURL;
 }
 
 @end
