0bb432bb007bbdd0005602ec411b543480c2b98f
wumaurice <wumaurice@hotmail.com>
2017-02-06 00:11:30 -0500

    Fix black video upload

--- Fresco News/SDAVAssetExportSession.h
+++ Fresco News/SDAVAssetExportSession.h
@@ -188,7 +188,7 @@
 @end
 
 @protocol SDAVAssetExportSessionDelegate <NSObject>
-
+@optional
 - (void)exportSession:(SDAVAssetExportSession *)exportSession renderFrame:(CVPixelBufferRef)pixelBuffer withPresentationTime:(CMTime)presentationTime toBuffer:(CVPixelBufferRef)renderBuffer;
 
 - (void)updateTranscodingProgress:(float)progress withPostID:(NSString *)postID;
