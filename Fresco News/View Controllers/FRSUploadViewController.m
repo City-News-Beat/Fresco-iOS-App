c6c56d94570890a88bb504e2ccaf7e92d8633672
wumaurice <wumaurice@hotmail.com>
2017-02-11 01:02:10 -0500

    Potential fix for item not found

--- Fresco News/View Controllers/FRSUploadViewController.m
+++ Fresco News/View Controllers/FRSUploadViewController.m
@@ -1254,7 +1254,6 @@ static NSString *const cellIdentifier = @"assignment-cell";
     NSArray *posts = postData[@"posts_new"];
 
     int i = 0;
-    [[FRSUploadManager sharedInstance] setUploadsToComplete:(int)self.content.count];
     for (PHAsset *asset in self.content) {
         NSDictionary *post = posts[i];
         NSString *key = post[@"key"];
