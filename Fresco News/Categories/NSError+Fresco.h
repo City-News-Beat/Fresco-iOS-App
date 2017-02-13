e954f58d17fe0f0f586005e91d122cc953bf84dd
Elmir Kouliev <elmir@kouliev.com>
2017-02-12 21:09:34 -0500

    fixed doc

--- Fresco News/Categories/NSError+Fresco.h
+++ Fresco News/Categories/NSError+Fresco.h
@@ -16,7 +16,7 @@
 
 
 /**
- Returns a clean, generic error with.
+ Returns a clean, generic error with descriptions and codes.
  Methods below this are just stripped down versions that lead to this one
 
  @param message message to return
