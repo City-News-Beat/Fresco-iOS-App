96df84ca68cabf23a26226383000dd4ba0334886
Omar Elfanek <omar.elfanek@gmail.com>
2017-05-01 17:21:36 -0400

    Camera footer view (wip)

---
+++ Fresco News/FRSCameraFooterView.h
@@ -0,0 +1,13 @@
+//
+//  FRSCameraFooterView.h
+//  Fresco
+//
+//  Created by Omar Elfanek on 5/1/17.
+//  Copyright © 2017 Fresco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRSCameraFooterView : UIView
+
+@end
---
+++ Fresco News/FRSCameraFooterView.m
@@ -0,0 +1,23 @@
+//
+//  FRSCameraFooterView.m
+//  Fresco
+//
+//  Created by Omar Elfanek on 5/1/17.
+//  Copyright © 2017 Fresco. All rights reserved.
+//
+
+#import "FRSCameraFooterView.h"
+
+@implementation FRSCameraFooterView
+
+-(instancetype)init {
+    self = [super init];
+    
+    if (self) {
+    
+    }
+    
+    return self;
+}
+
+@end
