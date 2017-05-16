40a0e934075d4548d39401f2a0fc45ad83fd6fff
Omar Elfanek <omar.elfanek@gmail.com>
2017-05-16 12:02:51 -0400

    Add and implement FRSTipsManager, abstract dictionary values, fix choppy scroll.

---
+++ Fresco News/View Controllers/FRSTipsManager.h
@@ -0,0 +1,13 @@
+//
+//  FRSTipsManager.h
+//  Fresco
+//
+//  Created by Omar Elfanek on 5/15/17.
+//  Copyright © 2017 Fresco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FRSTipsManager : NSObject
+
+@end
---
+++ Fresco News/View Controllers/FRSTipsManager.m
@@ -0,0 +1,13 @@
+//
+//  FRSTipsManager.m
+//  Fresco
+//
+//  Created by Omar Elfanek on 5/15/17.
+//  Copyright © 2017 Fresco. All rights reserved.
+//
+
+#import "FRSTipsManager.h"
+
+@implementation FRSTipsManager
+
+@end
