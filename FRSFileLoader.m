4891e4ae966503041ef46361e5ae22b145e3f7f2
Revanth Kumar Yarlagadda <revanth@fresconews.com>
2017-05-23 15:59:27 -0400

    Filter media - creation date of the asset to be mandatory in order to display in the available assets.

--- FRSFileLoader.m
+++ FRSFileLoader.m
@@ -15,41 +15,41 @@
 // only overrode so that we'd immediately load assets, and, by design, request authorization upon open
 - (id)init {
     self = [super init];
-
+    
     if (self) {
         [self getAssets];
     }
-
+    
     return self;
 }
 
 - (id)initWithDelegate:(id<FRSFileLoaderDelegate>)del {
     self = [super init];
-
+    
     if (self) {
         self.delegate = del;
-
+        
         if (![self checkAuthorization]) { // only request auth if we don't have auth
             [self getAuthorization];
         } else {
             [self getAssets]; // we have auth, loading list of assets
         }
     }
-
+    
     return self;
 }
 
 - (void)fetchAssetsWithinIndexRange:(NSRange)range callback:(MediaCallback)callback {
     currentRange = range;
-
+    
     if (![self checkAuthorization]) {
         [self getAuthorization];
     }
-
+    
     if (range.location + range.length >= [allAssets count]) {
         range.length = [allAssets count] - range.length - 1; // reached end of media
     }
-
+    
     NSArray *toReturn = [allAssets objectsAtIndexes:[NSIndexSet indexSetWithIndexesInRange:range]];
     callback(toReturn, Nil);
 }
@@ -58,7 +58,7 @@
     if (index >= [allAssets count]) {
         return Nil;
     }
-
+    
     return [allAssets objectAtIndex:index];
 }
 
@@ -71,43 +71,42 @@
     if (!currentCollection) {
         [self getAlbumCollection];
     }
-
+    
     if (!allAssets) { // discount doublecheck
         allAssets = [[NSMutableArray alloc] init];
     }
-
+    
     
     PHFetchOptions *options = [[PHFetchOptions alloc] init];
     // have most recently created assets at this top of list
     options.sortDescriptors = @[ [NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO] ];
     
     /* We don't want to limit file age to seven days anymore.
-    #if TARGET_OS_SIMULATOR
-        //Simulator
-    #else
-        //Device
-        // only load assets w/ creation date within the defined maximum age
-        NSDate *date = [[NSDate date] dateByAddingTimeInterval:-maxFileAge];
-        NSPredicate *dayPredicate = [NSPredicate predicateWithFormat:@"creationDate >= %@", date];
-        options.predicate = dayPredicate;
-    #endif
+     #if TARGET_OS_SIMULATOR
+     //Simulator
+     #else
+     //Device
+     // only load assets w/ creation date within the defined maximum age
+     NSDate *date = [[NSDate date] dateByAddingTimeInterval:-maxFileAge];
+     NSPredicate *dayPredicate = [NSPredicate predicateWithFormat:@"creationDate >= %@", date];
+     options.predicate = dayPredicate;
+     #endif
      */
-     
-
+    
+    
     for (PHAssetCollection *collection in currentCollection) {
         // fetch assets based on the sort and date restrictions we set up
         PHFetchResult *assets = [PHAsset fetchAssetsInAssetCollection:collection options:options];
-
+        
         // add each asset to our file list
         for (PHAsset *asset in assets) {
-            if (asset.duration <= 61) {
-                if (asset.location) {
-                    [allAssets addObject:asset];
-                }
+            //mandating the creation date also. And writing only one if condition that satisfies all requirements.
+            if (asset.duration <= 61 && asset.location && asset.creationDate) {
+                [allAssets addObject:asset];
             }
         }
     }
-
+    
     // delegate called to notify that we are authorized (only used first time user opens app, and gets the "Please allow access to photos" prompt
     if (self.delegate) {
         [self.delegate filesLoaded];
@@ -123,53 +122,53 @@
 - (void)getAuthorization {
     if ([allAssets count] > 0)
         return;
-
+    
     [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
-      switch (status) {
-      case PHAuthorizationStatusAuthorized:
-          [self getAssets];
-          if (!wasPreviouslyAuthorized) {
-              [FRSTracker track:photosEnabled];
-          }
-          break;
-      case PHAuthorizationStatusRestricted:
-          [self getAssets];
-          break;
-      case PHAuthorizationStatusDenied:
-          if (self.delegate) { // non-optional not checking for conformity
-              [self.delegate applicationNotAuthorized]; // can't re-ask, need to go into settings
-          }
-
-          if (wasPreviouslyAuthorized) {
-              [FRSTracker track:photosDisabled];
-          }
-          break;
-      default:
-          break;
-      }
+        switch (status) {
+            case PHAuthorizationStatusAuthorized:
+                [self getAssets];
+                if (!wasPreviouslyAuthorized) {
+                    [FRSTracker track:photosEnabled];
+                }
+                break;
+            case PHAuthorizationStatusRestricted:
+                [self getAssets];
+                break;
+            case PHAuthorizationStatusDenied:
+                if (self.delegate) { // non-optional not checking for conformity
+                    [self.delegate applicationNotAuthorized]; // can't re-ask, need to go into settings
+                }
+                
+                if (wasPreviouslyAuthorized) {
+                    [FRSTracker track:photosDisabled];
+                }
+                break;
+            default:
+                break;
+        }
     }];
 }
 
 // just checks if we're already authorized
 - (BOOL)checkAuthorization {
     __block BOOL authorized = FALSE;
-
+    
     switch ([PHPhotoLibrary authorizationStatus]) {
-    case PHAuthorizationStatusAuthorized:
-        authorized = TRUE;
-        break;
-    case PHAuthorizationStatusRestricted:
-        authorized = TRUE;
-        break;
-    case PHAuthorizationStatusDenied:
-        authorized = FALSE; // not necessary (default) but consistent
-        break;
-    default:
-        break;
+        case PHAuthorizationStatusAuthorized:
+            authorized = TRUE;
+            break;
+        case PHAuthorizationStatusRestricted:
+            authorized = TRUE;
+            break;
+        case PHAuthorizationStatusDenied:
+            authorized = FALSE; // not necessary (default) but consistent
+            break;
+        default:
+            break;
     }
-
+    
     wasPreviouslyAuthorized = authorized;
-
+    
     return authorized;
 }
 
@@ -183,18 +182,18 @@
     [[PHImageManager defaultManager] requestImageDataForAsset:phAsset
                                                       options:options
                                                 resultHandler:^(NSData *_Nullable imageData, NSString *_Nullable dataUTI, UIImageOrientation orientation, NSDictionary *_Nullable info) {
-
-                                                  if (phAsset.mediaType == PHAssetMediaTypeVideo) {
-                                                      [[PHImageManager defaultManager] requestAVAssetForVideo:phAsset
-                                                                                                      options:Nil
-                                                                                                resultHandler:^(AVAsset *_Nullable avAsset, AVAudioMix *_Nullable audioMix, NSDictionary *_Nullable info) {
-                                                                                                  callback([UIImage imageWithData:imageData], avAsset, phAsset.mediaType, error);
-                                                                                                }];
-
-                                                      return;
-                                                  }
-
-                                                  callback([UIImage imageWithData:imageData], Nil, phAsset.mediaType, error);
+                                                    
+                                                    if (phAsset.mediaType == PHAssetMediaTypeVideo) {
+                                                        [[PHImageManager defaultManager] requestAVAssetForVideo:phAsset
+                                                                                                        options:Nil
+                                                                                                  resultHandler:^(AVAsset *_Nullable avAsset, AVAudioMix *_Nullable audioMix, NSDictionary *_Nullable info) {
+                                                                                                      callback([UIImage imageWithData:imageData], avAsset, phAsset.mediaType, error);
+                                                                                                  }];
+                                                        
+                                                        return;
+                                                    }
+                                                    
+                                                    callback([UIImage imageWithData:imageData], Nil, phAsset.mediaType, error);
                                                 }];
 }
 
