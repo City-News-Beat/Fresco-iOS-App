894776a81c5c95768a2b091d86a04659dfc4951e
Revanth Kumar Yarlagadda <revanth@fresconews.com>
2017-06-20 17:03:43 -0400

    User Story - setup swift compatibility (included bridging header) and created first swift class FRSUserStoryManager

---
+++ Fresco News/FRSUserStoryManager.swift
@@ -0,0 +1,13 @@
+//
+//  FRSUserStoryManager.swift
+//  Fresco
+//
+//  Created by Revanth Kumar Yarlagadda on 6/20/17.
+//  Copyright © 2017 Fresco. All rights reserved.
+//
+
+import Foundation
+
+class FRSUserStoryManager: FRSBaseManager {
+
+}
---
+++ Fresco News/Headers/Fresco-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Fresco-Bridging-Header.h
+//  Fresco
+//
+//  Created by Revanth Kumar Yarlagadda on 6/20/17.
+//  Copyright © 2017 Fresco. All rights reserved.
+//
+
+#ifndef Fresco_Bridging_Header_h
+#define Fresco_Bridging_Header_h
+
+#import <UIKit/UIKit.h>
+#import "FRSBaseManager.h"
+
+#endif /* Fresco_Bridging_Header_h */
